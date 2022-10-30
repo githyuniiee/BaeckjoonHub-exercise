8ff6392c2df7444dad0c31786e0acc559dcc758b
githyuniiee <109260733+githyuniiee@users.noreply.github.com>
2022-10-30 22:31:49 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	
+	public static void main (String[] args) {
+		Scanner keyboard = new Scanner(System.in); 
+		int A, B;
+		A = keyboard.nextInt();
+		B = keyboard.nextInt();
+		System.out.println((double)A/B);
+		 
+		 
+	}
+	
+}
