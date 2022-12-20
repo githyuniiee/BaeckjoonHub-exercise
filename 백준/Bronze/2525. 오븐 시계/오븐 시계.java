231986f946b90e1b30be48040991518fbecd59fc
Bohyun Choi <109260733+githyuniiee@users.noreply.github.com>
2022-12-20 23:05:05 +0900

    [Bronze III] Title: 오븐 시계, Time: 228 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
-public class Main {
 
-	
-	public static void main (String[] args) {
-	Scanner keyboard = new Scanner(System.in);
-	
-	int A = keyboard.nextInt();
-	int B = keyboard.nextInt();
-	int C = keyboard.nextInt();
-	
-	int min = 60 *A + B;
-	min = min + C;
-	
-	int hour = (min/60) % 24;
-	int minute = min%60;
-	
-	System.out.println(hour + " " + minute);
-	
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		int A = keyboard.nextInt();
+		int B = keyboard.nextInt();
+		int C = keyboard.nextInt();
+		
+		int time = A*60 +B;
+		time += C;
+		
+		A = (time/60)%24;
+		B = time%60;
+		
+		
+		System.out.print(A + " " + B);
 	}
-	
 }
