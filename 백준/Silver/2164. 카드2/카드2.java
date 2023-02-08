c2ec56efd9bcc478c01eef417bc23ab5e28b4c00
Bohyun Choi <109260733+githyuniiee@users.noreply.github.com>
2023-02-08 18:55:14 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 43284 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,35 @@
+import javax.sound.sampled.Line;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=1; i<=N; i++){
+            q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(q.size() != 1){
+            q.poll();
+            q.add(q.poll());
+
+
+        }
+        sb.append(q.poll());
+        System.out.println(sb);
+
+
+
+    }
+}
