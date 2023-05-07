47b63aa46cc66d3d23c2cd019ec72755d9ec5598
Bohyun Choi <109260733+githyuniiee@users.noreply.github.com>
2023-05-07 16:20:35 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        int len = arr.length;
+        
+        double answer = 0;
+        for(int i=0; i<len; i++){
+            answer += arr[i];
+        }
+        answer /= len;
+        return answer;
+    }
+}
