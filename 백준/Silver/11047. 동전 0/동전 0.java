7721a4bebcfaef9fd483d1ea162794a9b2b078f3
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-07-23 17:07:35 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= k) {
+                result += k / arr[i];
+                k = k % arr[i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
