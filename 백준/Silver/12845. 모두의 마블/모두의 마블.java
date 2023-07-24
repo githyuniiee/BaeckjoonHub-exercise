86102c67e88bfc06b032be89cd7bfd6bdaf59841
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-07-24 09:02:22 +0900

    [Silver III] Title: 모두의 마블, Time: 92 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer[]init = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            init[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(init, Collections.reverseOrder()); //내림차순 정렬
+
+        int maxLevel = init[0];
+        int[] gold = new int[n - 1];
+        gold[0] = init[0] + init[1];
+        int sum = 0;
+
+
+        for (int i = 1; i < n - 1; i++) {
+            gold[i] = init[i + 1] + maxLevel;
+        }
+
+        for (int j = 0; j < n-1; j++) {
+            sum += gold[j];
+        }
+
+        System.out.println(sum);
+
+    }
+}
