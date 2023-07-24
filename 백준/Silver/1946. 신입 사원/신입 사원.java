1597333242d69ae92135b1d20efb902cd676d51f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-07-24 19:48:44 +0900

    [Silver I] Title: 신입 사원, Time: 1548 ms, Memory: 302464 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,54 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Grade implements Comparable<Grade> {
-	int a;
-	int b;
+    int a;
+    int b;
 
-	Grade(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
+    Grade(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
-	@Override
-	public int compareTo(Grade o) {
-		if(this.a > o.a) {
-			return 1;
-		} else {
-			return -1;
-		}
-	}
+    @Override
+    public int compareTo(Grade o) {
+        if (this.a > o.a) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-		for(int t = 0; t < T; t++) {
-			int N = sc.nextInt();
-			ArrayList<Grade> list = new ArrayList<>();
+        ArrayList<Integer> results = new ArrayList<>();
 
-			for(int i = 0; i < N; i++) {
-				int a = sc.nextInt();
-				int b = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            ArrayList<Grade> list = new ArrayList<>();
 
-				list.add(new Grade(a, b));
-			}
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
 
-			Collections.sort(list); // 서류 점수 기준 오름차순 정렬 
+                list.add(new Grade(a, b));
+            }
 
-			int ans = 1; // 서류 1등은 무조건 통과 
-			int min = list.get(0).b; // 면접 점수 최소값 
-			for(int i = 1; i < N; i++) { // 서류 2등부터 시작 
-				if(list.get(i).b < min) { // 이전의 최소 면접 점수보다 낮으면 통과 
-					ans++;
-					min = list.get(i).b; // 최소 점수 갱신 
-				}
-			}
-			System.out.println(ans);
-		}	
-	}
+            Collections.sort(list); // 서류 점수 기준 오름차순 정렬
+
+            int ans = 1; // 서류 1등은 무조건 통과
+            int min = list.get(0).b; // 면접 점수 최소값
+            for (int i = 1; i < N; i++) { // 서류 2등부터 시작
+                if (list.get(i).b < min) { // 이전의 최소 면접 점수보다 낮으면 통과
+                    ans++;
+                    min = list.get(i).b; // 최소 점수 갱신
+                }
+            }
+            results.add(ans);
+        }
+
+        // Print the results
+        for (int result : results) {
+            System.out.println(result);
+        }
+    }
 }
