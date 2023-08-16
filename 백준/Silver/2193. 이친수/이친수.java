b0733bc1810dc662100ca411f20206603b11eb61
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-08-16 11:33:37 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,24 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+
+
 
 public class Main {
-    static int N;
-    static long[][] dp;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        dp = new long[N + 1][2];
-        dp[1][1] = 1;
+        long[][] dp = new long[n + 1][2];
         dp[1][0] = 0;
-
-        for (int i = 2; i <= N; i++) {
-            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-            dp[i][1] = dp[i - 1][0];
+        dp[1][1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= 1; j++) {
+                if(j==0){
+                    dp[i][j] = dp[i-1][0] + dp[i-1][1];
+                } else if (j == 1) {
+                    dp[i][j] = dp[i - 1][0];
+                }
+            }
         }
-        System.out.println(dp[N][0] + dp[N][1]);
+        long answer = 0;
+        answer = dp[n][0] + dp[n][1];
+        System.out.println(answer);
     }
 }
