b82f4a685de651780d83598fc7e0b094607d9109
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-10-30 00:36:44 +0900

    [level unrated] Title: 배열 자르기, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -1,12 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
       
-        int[] answer = new int[num2-num1+1];
-        int k = 0;
-        
-        for(int i= num1; i<=num2; i++){
-            answer[k++] = numbers[i];
-        }
-        return answer;
+     
+        return Arrays.copyOfRange(numbers, num1, num2 +1);
     }
 }
