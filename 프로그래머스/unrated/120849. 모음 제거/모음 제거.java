c800a4156b0707715e05400799db78147163b356
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2023-11-04 14:56:34 +0900

    [level unrated] Title: 모음 제거, Time: 0.11 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -3,28 +3,11 @@ import java.lang.*;
 class Solution {
     public String solution(String my_string) {
     
-        char arr[] = new char[]{'a', 'e', 'i', 'o', 'u'};
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
-        for(int i=0; i<my_string.length(); i++){
-            
-            char ch = my_string.charAt(i);
-            boolean bl = false;
-            
-            for(int j=0; j<arr.length; j++){
-                
-                if(ch == arr[j]){
-                    bl = true;
-                    break;
-                
-            }
-            }
-            
-            if(!bl){
-                sb.append(ch);
-            }
-        }
-        return sb.toString();
+        answer = my_string.replaceAll("[aeiou]", "");
+        
+        return answer;
 
     }
 }
