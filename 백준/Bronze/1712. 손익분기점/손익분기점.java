a148404a4a6bc7b1302106e348294a055086a450
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-01-02 11:22:00 +0900

    [Bronze II] Title: 손익분기점, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if( c <= b){
+            System.out.println("-1");
+        }else {
+
+            System.out.println((a / (c - b)) + 1);
+        }
+        
+    }
+}
