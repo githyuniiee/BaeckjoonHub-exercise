f495efc05a32a91ff43d1e7110ed77f8ce21d603
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-01-04 09:49:24 +0900

    [Bronze II] Title: 상수, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -8,27 +8,10 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String a = st.nextToken();
-        String b = st.nextToken();
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = a.length() -1 ; i >= 0; i--) {
-            sb.append(a.charAt(i));
-        }
-
-        StringBuilder sb1 = new StringBuilder();
-        for (int i = b.length() -1 ; i >= 0; i--) {
-            sb1.append(b.charAt(i));
-        }
-
-        int r1 = Integer.parseInt(String.valueOf(sb));
-        int r2 = Integer.parseInt(String.valueOf(sb1));
-
-        if(r1 < r2){
-            System.out.println(r2);
-        }else{
-            System.out.println(r1);
-        }
+        System.out.println(A > B ? A : B);
 
     }
 }
