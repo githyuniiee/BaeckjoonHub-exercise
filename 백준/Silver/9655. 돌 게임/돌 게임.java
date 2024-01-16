05667253d814854d6ed36e8b2b5ab1fcf4372165
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-01-16 22:53:07 +0900

    [Silver V] Title: 돌 게임, Time: 80 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n%2==0 ? "CY" : "SK");
+
+
+
+
+
+
+
+    }
+}
