b8016906bdb9fe0dc88c0a9b607642fe14cf09c3
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-01-29 15:10:37 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-      
-        int answer = n-lost.length;
+     
+        int[] people = new int[n];
+        int answer = n;
         
-        //정렬
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        for(int l : lost)
+            people[l-1]--;
         
-        //여벌 체육복을 가져온 학생이 도난당한 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
+        for(int r : reserve)
+            people[r-1]++;
+        
+        
+        for(int i=0; i<people.length; i++){
+            if(people[i] == -1){
+                if(i-1>=0 && people[i-1] == 1){
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1<people.length && people[i+1] == 1){
+                    people[i]++;
+                    people[i+1]--;
+                }else{
+                    answer--;
                 }
             }
         }
         
-        //도난당한 학생에게 체육복 빌려주는 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i]+1 == reserve[j] || lost[i]-1 == reserve[j]){
-                    answer ++;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
-        }
         return answer;
     }
 }
