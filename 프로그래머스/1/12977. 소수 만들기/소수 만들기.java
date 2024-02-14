566ef1ffacc7a3e5df3bbcf2a8b319e175bb2366
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-02-14 21:10:18 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        boolean chk = false;
+        
+        for(int i=0; i<nums.length-2; i++){
+            for(int j=i+1; j<nums.length-1; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int num = nums[i] + nums[j] + nums[k];
+                    if(num >= 2) chk = sosu(num);
+                    if(chk == true) answer++;
+                 
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean sosu(int num){
+        boolean check = true;
+        if(num == 2) return check;
+        
+        for(int i=2; i<num; i++){
+            if(num%i == 0){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+}
