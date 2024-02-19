4f911b388f6cf270f01cfca5ba2f71ee00847f01
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-02-19 09:12:53 +0900

    [level 1] Title: 실패율, Time: 2.11 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,41 +1,50 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int N, int[] stages) {
-       int[] answer = new int[N];
-        Map<Integer, Double> fail = new HashMap<>();
+    public  int[] solution(int N, int[] stages) {
+        double[] failRate = new double[N];
+        double remain = stages.length;
+        int stage = 1; //현재 스테이지
+        double failNum; //각 스테이지 별 실패 한 사용자 수
+        int[] answer = new int[N];
         
-        for(int i=1; i<= N; i++) {
-            int challenger = 0; //도전자 수
-            int failUser = 0; //실패한 사용자 수
-            for(int j=0; j< stages.length; j++){
-                if(i <= stages[j]){
-                    challenger++;
-                }
-                if(i == stages[j]) {
-                    failUser++;
-                }
-            }
+        for(int j=0; j<N; j++){
+            failNum = 0;
+            for(int i=0; i<stages.length; i++){
             
-            fail.put(i, challenger == 0 ? 0 : ((double) failUser / challenger));
-        }
-        
-        //실패율 정렬
-        List<Map.Entry<Integer, Double>> entryList = new ArrayList<>(fail.entrySet());
-        entryList.sort((o1, o2) -> {
-            if(o1.getValue().equals(o2.getValue())) {
-                return o1.getKey() - o2.getKey();
-            } else {
-                return o2.getValue(). compareTo(o1.getValue());
+            if(stages[i] == stage){
+                failNum ++;
             }
-        });
-        
-        //결과 반환
-        int idx = 0;
-        for(Map.Entry<Integer, Double> map : entryList) {
-            answer[idx++] = map.getKey();
+            }
+            if(remain != 0){
+            failRate[j] =  failNum / remain;
+            remain -= failNum;
+            } else{
+                failRate[j] = 0;
+            }
+            stage++;
         }
+        
+        //Map에 담기
+        Map<Integer, Double> map = new HashMap<>();
+        
+        for(int i=0; i<N; i++){
+            map.put(i+1, failRate[i]);
+        }
+        
+        // value 기준 내림차순 정렬
+        List<Integer> sortedKeys = new ArrayList<>(map.keySet());
+        sortedKeys.sort(Comparator.comparingDouble(map::get).reversed());
+        
+        // List<Double> valueList = new ArrayList<>(map.values());
+        // valueList.osrt((v1, v2) -> v2.compareTo(v1));
+        
+        int index = 0;
+        for(Integer num : sortedKeys){
+            answer[index++]= num;
+        }
+        
         
         return answer;
-        
+    
     }
 }
