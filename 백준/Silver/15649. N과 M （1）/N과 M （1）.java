51809997f42bac32687aaae715c8189b7252d33d
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-02-23 10:48:42 +0900

    [Silver III] Title: N과 M (1), Time: 2460 ms, Memory: 67116 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static int[] arr;
-    static boolean[] visit;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static int n,m;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
 
-        visit = new boolean[N + 1];
-        arr = new int[N + 1];
-
-        dfs(0);
-    }
-
-    public static void dfs(int idx){
-        if(idx == M){
-            for (int i = 0; i < M; i++) {
-                System.out.print(arr[i]+ " ");
+    public static void dfs(int num){
+        if(num == m){
+            for(int a : arr){
+                System.out.print(a + " ");
             }
             System.out.println();
-
+            return;
         }
-        for(int i=1; i<=N; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                arr[idx] = i;
-                dfs(idx+1);
-                visit[i] = false;
 
+        for(int i=0; i<n; i++){
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[num] = i+1;
+                dfs(num+1);
+                visit[i] = false;
             }
+
         }
 
     }
 
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visit = new boolean[n];
+
+        dfs(0);
+        System.out.println(sb);
+
+
+
+
+    }
 }
