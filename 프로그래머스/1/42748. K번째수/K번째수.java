0550f18d0ecf77d9e06ca560243edf0e88b3074d
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-02-27 07:59:58 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,29 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-      
-        ArrayList<Integer> arr =new ArrayList<>();
+        
         int[] answer = new int[commands.length];
         
-        for(int l=0; l<commands.length; l++){
-            int i = commands[l][0];
-            int j = commands[l][1];
-            int k = commands[l][2];
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             
-            //i부터 j까지
-            for(int a = i-1 ; a < j; a++){
-                arr.add(array[a]);
-            }
-            
-            //정렬
-            Collections.sort(arr);
-            
-            //반환 배열
-            answer[l] = arr.get(k-1);
-            arr.clear();
-            
-            
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
         }
+        
         return answer;
+        
     }
 }
