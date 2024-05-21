98633f87a18dc1c6adedc0c272491df9791b1261
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-05-21 17:53:41 +0900

    [Silver V] Title: K번째 수, Time: 3344 ms, Memory: 663860 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,33 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int total = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
+
         st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-
-       
-        if( 1 <= K && K <= N ) {
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(arr);
-
-            System.out.println(arr[K - 1]);
-
+        for (int i = 0; i < total; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
         }
-    }
 
+        Collections.sort(list);
+
+        System.out.println(list.get(n-1));
+
+
+    }
 }
