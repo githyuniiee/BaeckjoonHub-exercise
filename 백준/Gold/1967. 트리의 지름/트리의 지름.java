54724d02b046e042484b611b5a92ca2ee5902d08
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-05-23 14:25:18 +0900

    [Gold IV] Title: 트리의 지름, Time: 2580 ms, Memory: 135532 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static  int n;
+    static List<Node> nodes[];
+    static boolean[] visited;
+    static int max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nodes = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            nodes[from].add(new Node(to,value));
+            nodes[to].add(new Node(from,value));
+        }
+
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
+            visited[i] = true;
+            dfs(i, 0);
+        }
+        System.out.println(max);
+    }
+
+    public static void dfs(int num, int value) {
+        for (Node node : nodes[num]) {
+            if (!visited[node.link]) {
+                visited[node.link] = true;
+                dfs(node.link, value + node.value);
+            }
+        }
+        max = Math.max(max, value);
+    }
+
+    static class Node {
+        public int link;
+        public int value;
+
+        public Node(int link, int value) {
+            this.link = link;
+            this.value = value;
+        }
+    }
+
+
+
+
+}
