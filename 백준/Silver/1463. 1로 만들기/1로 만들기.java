28e37f4f7dae4da0947c70dc6d253cfb8dbd5427
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-05-24 16:37:59 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static int d[];
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
-        d = new int[number + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
 
-        d[1] = 0;
-        for (int i = 2; i < number + 1; i++) {
+        dp[0] = 0;
+        dp[1] = 0;
 
-            d[i] = d[i-1] + 1;
-            if (i % 2 == 0 && d[i] > d[i / 2] + 1) {
-                d[i] = d[i/2]+ 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] +1;
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            if (i % 3 == 0 && d[i] > d[i / 3] + 1) {
-                d[i] = d[i/3] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
-        System.out.println(d[number]);
+
+        System.out.println(dp[n]);
+
 
 
     }
+
 }
