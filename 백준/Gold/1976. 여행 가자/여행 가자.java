de9d65ad290af9bee1d31d921a026f6450c5424c
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-05-29 17:42:51 +0900

    [Gold IV] Title: 여행 가자, Time: 144 ms, Memory: 16552 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+public class Main {
+
+    static int graph[];
+
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        graph = new int[n+1];
+        for(int i=1; i<=n; i++){
+            graph[i] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            String[] str = br.readLine().split(" ");
+            for(int j=0; j<str.length; j++){
+                if(Integer.parseInt(str[j]) == 1) {
+                    union(i, j+1);
+                }
+            }
+        }
+
+        String[] str = br.readLine().split(" ");
+        boolean check = true;
+        for(int i=0; i<str.length-1; i++){
+             int a = Integer.parseInt(str[i]);
+             int b = Integer.parseInt(str[i+1]);
+             if(find(a) != find(b)){
+                 check = false;
+                 break;
+             }
+        }
+
+        if(check){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+    static int find(int x){
+        if(graph[x] != x){
+            graph[x] = find(graph[x]);
+        }
+        return graph[x];
+    }
+
+    static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB){
+            graph[rootB] = rootA;
+        }
+    }
+
+}
