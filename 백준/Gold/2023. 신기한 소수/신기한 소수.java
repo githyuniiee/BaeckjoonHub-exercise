d7907d3d33e59bc7aecd9c69cf422ad4da320bf5
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-06-10 11:35:33 +0900

    [Gold V] Title: 신기한 소수, Time: 72 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,34 +1,56 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int n;
+
+    public static boolean isPrime(int num){
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0){
+                return false; //소수 아님
+            }
+        }
+
+        return true; //소수 임
+
+    }
+
+    public static void dfs(int num, int jarisu) {
+
+        if (jarisu == n) {
+            if (isPrime(num)) {
+                System.out.println(num);
+            }
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            if (isPrime(num * 10 + i)) {
+                dfs(num * 10 + i, jarisu + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        dfs(0, n);
-    }
+        n = Integer.parseInt(br.readLine());
 
-    static void dfs(int num, int n) {
-        if(n==0) {
-            System.out.println(num);
-        }
-        for(int i=1; i<10; i++) {
-            int tmp = 10*num +i;
-            if(n>0 && isPrime(tmp)) {
-                dfs(tmp, n-1);
-            }
-        }
-    }
+        //일의 자리 소수 2,3,5,7 4개 수
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
 
-    static boolean isPrime(int num) {
-        if(num <2) return false;
-        for(int i=2 ; i*i<=num; i++) {
-            if(num%i ==0) {
-                return false; //num이 i의 배수면 소수가 아니므로 false
-            }
-        }
-        return true;
     }
 }
+
+
+
+
+
