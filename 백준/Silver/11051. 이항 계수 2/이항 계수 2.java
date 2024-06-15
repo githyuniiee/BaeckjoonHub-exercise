9b3e7b22db065f4ec645fa3c7fac3ac5b063bf91
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-06-16 08:32:34 +0900

    [Silver II] Title: 이항 계수 2, Time: 92 ms, Memory: 15768 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -1,30 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n+1][n+1];
 
-        int[][] dp = new int[n+1][n+1];
         for (int i = 0; i <= n; i++) {
-            dp[i][i] = 1;
-            dp[i][1] = i;
-            dp[i][0] = 1;
+            arr[i][0] = 1;
+            arr[i][1] = 1;
+            arr[i][i] = 1;
         }
 
-        for(int i=2; i<=n; i++){
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j < i; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) %10007;
+                arr[i][j] = (arr[i - 1][j] + arr[i - 1][j - 1]) % 10007;
             }
         }
 
-        System.out.println(dp[n][k]);
 
-
+        System.out.println(arr[n][k]);
 
 
 
