4393adfd43e56b458cd6b1442e75d902f00dc0a2
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-06-20 08:01:50 +0900

    [Bronze II] Title: 공 바꾸기, Time: 76 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+
+        // Initialize the array with values from 1 to n
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+        }
+
+        for (int j = 1; j <= n; j++) {
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
