91ab79790800a01f865b5b40b7e5a4d73c4853ac
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-06-21 22:01:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 1996 ms, Memory: 65196 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Person[] p = new Person[n];
+
+
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            p[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
+        }
+
+        Arrays.sort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        for (Person ps : p) {
+            int a = ps.age;
+            String b = ps.name;
+            System.out.println(a + " " + b);
+        }
+
+
+
+    }
+
+    public static class Person {
+        int age;
+        String name;
+
+        Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
+
