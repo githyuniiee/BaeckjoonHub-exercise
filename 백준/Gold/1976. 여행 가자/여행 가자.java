e563c8a93a493d11120f80f084fa6422ffefaefe
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-06-26 08:16:15 +0900

    [Gold IV] Title: 여행 가자, Time: 140 ms, Memory: 16696 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,9 +1,11 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int graph[];
-
 
     public static void main(String[] args) throws Exception {
 
@@ -11,51 +13,52 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        graph = new int[n+1];
-        for(int i=1; i<=n; i++){
+        graph = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             graph[i] = i;
         }
 
+        // 신뢰 관계 입력
         for (int i = 1; i <= n; i++) {
             String[] str = br.readLine().split(" ");
-            for(int j=0; j<str.length; j++){
-                if(Integer.parseInt(str[j]) == 1) {
-                    union(i, j+1);
+            for (int j = 0; j < n; j++) {
+                if (Integer.parseInt(str[j]) == 1) {
+                    union(i, j + 1); // i와 j+1을 연결
                 }
             }
         }
 
+        // 여행 계획 입력
         String[] str = br.readLine().split(" ");
         boolean check = true;
-        for(int i=0; i<str.length-1; i++){
-             int a = Integer.parseInt(str[i]);
-             int b = Integer.parseInt(str[i+1]);
-             if(find(a) != find(b)){
-                 check = false;
-                 break;
-             }
+        for (int i = 0; i < str.length - 1; i++) {
+            int a = Integer.parseInt(str[i]);
+            int b = Integer.parseInt(str[i + 1]);
+            if (find(a) != find(b)) {
+                check = false;
+                break;
+            }
         }
 
-        if(check){
+        if (check) {
             System.out.println("YES");
-        }else{
+        } else {
             System.out.println("NO");
         }
     }
 
-    static int find(int x){
-        if(graph[x] != x){
+    static int find(int x) {
+        if (graph[x] != x) {
             graph[x] = find(graph[x]);
         }
         return graph[x];
     }
 
-    static void union(int a, int b){
+    static void union(int a, int b) {
         int rootA = find(a);
         int rootB = find(b);
-        if(rootA != rootB){
+        if (rootA != rootB) {
             graph[rootB] = rootA;
         }
     }
-
 }
