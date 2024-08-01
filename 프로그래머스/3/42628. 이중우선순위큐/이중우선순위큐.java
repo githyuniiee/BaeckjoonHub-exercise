71e9e0e63c92243b1babb0e0586ca2830e44c8ee
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-08-01 20:55:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 106.97 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,51 +3,44 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
     
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        
-        for(int i=0; i<operations.length; i++){
-            if(operations[i].charAt(0) == 'I'){
-                
-                //문자 -> 숫자
-                String str = operations[i].substring(2, operations[i].length());
-                int val = Integer.parseInt(str);
-                
-                //큐 삽입
-                queue.add(val);
-                
-                
-            }else if(operations[i].charAt(0) == 'D' && operations[i].charAt(2) == '1'){
-                
-                // 큐에서 최댓값 삭제
-                if(!queue.isEmpty()) {
-                    queue.remove(Collections.max(queue));
-                }
-                
-            }else{
-                
-               //최솟값 삭제
-                if(!queue.isEmpty()){
-                    queue.poll();
-                }
-    
-                
-            }
-            
-        }
-        
+        PriorityQueue<Integer> min = new PriorityQueue<>(); //오름차순
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         int[] answer = new int[2];
         
-        
-        //큐에서 최댓값 최솟값 
-        if(queue.isEmpty()){
-            answer[0] = 0;
-            answer[1] = 0;
-        }else{
-            answer[0] = Collections.max(queue);
-            answer[1] = Collections.min(queue);
+        for(String str : operations){
+            String[] arr = str.split(" ");
+            int num = Integer.parseInt(arr[1]);
+         
+            if(arr[0].equals("I")){
+                min.add(num);
+                max.add(num);
+                
+                continue;
+            }
+            
+            if(arr[0].equals("D") && !min.isEmpty() && arr[1].equals("1")) {
+                
+                int a = max.poll();
+                min.remove(a);
+            }else if(arr[0].equals("D") && !max.isEmpty() && arr[1].equals("-1")){
+                
+                int a = min.poll();
+                max.remove(a);
+            }
         }
         
+        
+        if(min.isEmpty()){
+            answer[0] = 0;
+            answer[1] = 0;
+            return answer;
+        }
+        
+        answer[0] = max.poll();
+        answer[1] = min.poll();
+        
         return answer;
+        
         
     }
 }
