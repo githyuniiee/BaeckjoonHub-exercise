5e1ddfb4c4978c5c1435da38ff315c8ec5253fd0
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-01 18:44:35 +0900

    [level 2] Title: 프로세스, Time: 1.34 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,33 +1,50 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
+    
+    static class Node{
+        int key;
+        int value;
         
-        //우선순위 큐 선언
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        public Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        for(int i : priorities){
-            queue.add(i);
+        // 인쇄 큐 초기화
+        Queue<Node> queue = new LinkedList<>();
+        
+        for(int i = 0; i < priorities.length; i++) {
+            queue.add(new Node(i, priorities[i]));
         }
         
-        //큐가 빌 때까지 반복
-        while(!queue.isEmpty()){
-            //기존 우선순위 배열 순회
-            for(int i=0; i<priorities.length; i++){
-                //현재 작업의 위치 찾기
-                    if(queue.peek() == priorities[i]){
-                        queue.poll();
-                        answer++;
-                        
-                        //현재 작업이 location과 같으면 answer 반환
-                        if(location == i){
-                            return answer;
-                        }
-                    }
+        // 우선순위 큐는 역순으로 정렬된 상태로 유지
+        Arrays.sort(priorities);
+        
+        int index = priorities.length - 1;
+        
+        while(!queue.isEmpty()) {
+            Node current = queue.poll();
+            
+            // 현재 노드의 우선순위가 가장 높은 우선순위와 일치하는지 확인
+            if(current.value == priorities[index]) {
+                answer++;
+                index--;
+                
+                // 만약 이 노드가 우리가 찾고 있는 위치에 있다면 종료
+                if(current.key == location) {
+                    break;
+                }
+            } else {
+                // 우선순위가 더 높은 문서가 뒤에 있으므로 현재 문서를 다시 큐의 맨 뒤로 추가
+                queue.add(current);
             }
         }
-        return answer;
         
+        return answer;
     }
 }
