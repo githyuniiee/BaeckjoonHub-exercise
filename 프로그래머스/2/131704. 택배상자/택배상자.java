ce78d2ad7e2f5043e81d2327cfd1a1225526c3fd
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-07 22:08:45 +0900

    [level 2] Title: 택배상자, Time: 52.18 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,27 +1,30 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public int solution(int[] order) {
-      
-        int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        int size = order.length;
-        int index = 0;
-        for(int i=1; i<=size; i++){
-            if(order[index] != i){
-                stack.push(i);
-            }else{
-                index++;
-                answer++;
+        int currentBox = 1; // 메인 컨테이너 벨트에서 나오는 상자의 번호
+        int answer = 0; // 트럭에 실은 상자의 개수
+        
+        for (int i = 0; i < order.length; i++) {
+            int targetBox = order[i]; // 트럭에 실어야 할 상자 번호
+            
+            // 메인 컨테이너 벨트에서 상자를 꺼내며 targetBox에 도달할 때까지 stack에 넣음
+            while (currentBox <= targetBox) {
+                stack.push(currentBox);
+                currentBox++;
             }
             
-            while(!stack.isEmpty() && stack.peek() == order[index]){
-                stack.pop();
-                index++;
+            // 스택의 맨 위에 있는 상자가 우리가 실어야 할 상자와 같은지 확인
+            if (!stack.isEmpty() && stack.peek() == targetBox) {
+                stack.pop(); // 스택에서 상자를 꺼내 트럭에 실음
                 answer++;
+            } else {
+                // 더 이상 상자를 실을 수 없으므로 종료
+                break;
             }
         }
+        
         return answer;
-
     }
 }
