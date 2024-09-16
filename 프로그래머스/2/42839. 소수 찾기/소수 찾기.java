0a479288b478ad5b2f9661c1afe6bd56cf93076f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-16 16:48:07 +0900

    [level 2] Title: 소수 찾기, Time: 28.87 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,11 +1,13 @@
 import java.util.*;
+
 class Solution {
     static Set<Integer> set;
-    static boolean[] visited = new boolean[7];
-    
+    static boolean[] visited;
     public int solution(String numbers) {
         int answer = 0;
         set = new HashSet<>();
+        visited = new boolean[7];
+        
         dfs(numbers, "", 0);
         
         for(Integer num : set){
@@ -17,36 +19,37 @@ class Solution {
         return answer;
     }
     
-    public static void dfs(String numbers, String s, int depth){
+    public void dfs(String numbers, String str, int depth){
         if(depth > numbers.length()){
             return;
         }
         
         for(int i=0; i<numbers.length(); i++){
-            
             if(!visited[i]){
                 visited[i] = true;
-                set.add(Integer.parseInt(s + numbers.charAt(i)));
-                dfs(numbers, s + numbers.charAt(i), depth+1);
+                set.add(Integer.parseInt(str + numbers.charAt(i)));
+                dfs(numbers, str+numbers.charAt(i), depth +1);
                 visited[i] = false;
             }
         }
     }
     
-    public static boolean isPrime(int n){
-        if(n < 2){
+    
+    
+    
+    public boolean isPrime(int num){
+        
+        if(num <2){
             return false;
         }
         
-        //에라토스테네스체
-        
-        for(int i=2; i<=(int)Math.sqrt(n); i++){
-            if(n%i == 0){
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i == 0){
                 return false;
             }
         }
         
         return true;
+        
     }
-    
 }
