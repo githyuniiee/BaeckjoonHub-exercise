de26193c56694575c174e2088a4b9da5d7c2806e
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-17 22:13:39 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.02 ms
+메모리: 73.9 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 20:01:30
+2024년 09월 17일 22:13:34
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,26 +1,31 @@
 class Solution {
     public int solution(int storey) {
+        
         int answer = 0;
         
         while(storey > 0){
             int digit = storey % 10;
-            storey = storey / 10;
+            storey /= 10;
             
             if(digit == 5){
+                
                 if(storey % 10 >= 5){
-                    answer = answer + (10 - digit);
+                    answer += 10 - digit;
                     storey++;
                 }else{
-                    answer = answer + digit;
+                    answer += digit;
                 }
-            }
-            else if(digit > 5){
-                answer = answer + (10-digit);
-                storey ++;
+                
+            }else if(digit > 5){
+                
+                answer += 10 - digit;
+                storey++;
+                
             }else{
-                answer = answer + digit;
+                answer += digit;
             }
         }
+        
         return answer;
     }
 }
