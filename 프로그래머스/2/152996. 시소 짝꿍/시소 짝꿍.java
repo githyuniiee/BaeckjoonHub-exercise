1bbfdfc1484bb3f678f41734e8c57475197f6b8f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-19 08:43:40 +0900

    [level 2] Title: 시소 짝꿍, Time: 91.01 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,37 +1,25 @@
 import java.util.*;
-
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
         Arrays.sort(weights);
-        // 100억번 연산 수행 -> 시간 초과!!!
+        Map<Double, Integer> map = new HashMap<>();
         
-        // if weight[i] <= weight[j]
-        // 1. weight[i] == weight[j]
-        // 2. weight[i] * 2 == weight[j]
-        // 3. weight[i] * 3 == weight[j] * 2
-        // 4. weight[i] * 4 == weight[j] * 3
-        
-        // for(int i = 0; i < weights.length - 1; i++){
-        //     for(int j = i + 1; j < weights.length; j++){
-        //         if(weights[i] == weights[j] ||
-        //           weights[i] * 2 == weights[j] ||
-        //           weights[i] * 3 == weights[j] * 2 ||
-        //           weights[i] * 4 == weights[j] * 3){
-        //             answer++;
-        //         }
-        //     }
-        // }
-        
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int weight : weights){
-            if(map.containsKey(weight)) answer += map.get(weight);
-            if(weight % 2 == 0 && map.containsKey(weight / 2)) answer += map.get(weight / 2);
-            if(weight % 3 == 0 && map.containsKey(weight * 2 / 3)) answer += map.get(weight * 2 / 3);
-            if(weight % 4 == 0 && map.containsKey(weight * 3 / 4)) answer += map.get(weight * 3 / 4);
-            map.put(weight, map.getOrDefault(weight, 0) + 1);
+        for(int i : weights){
+            double a = i*1.0;
+            double b = (i*2.0)/3.0;
+            double c = (i*1.0)/2.0;
+            double d = (i*3.0)/4.0;
+            
+            if(map.containsKey(a)) answer += map.get(a);
+            if(map.containsKey(b)) answer += map.get(b);
+            if(map.containsKey(c)) answer += map.get(c);
+            if(map.containsKey(d)) answer += map.get(d);
+            
+            map.put((i*1.0), map.getOrDefault((i*1.0),0) + 1);
+             
         }
+        
         return answer;
     }
 }
