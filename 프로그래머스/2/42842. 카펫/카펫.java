5a74ca5e41929aa7b28b8195eb5acb866490350a
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-25 22:14:39 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-    
         int[] answer = new int[2];
+        int carpet = brown + yellow;
         
-        //노란색 격자 수 1 이상 -> 가로, 세로 3 이상
-        for(int i=3; i< brown + yellow; i++) {
-            int width = (brown + yellow) / i;
-            //가로가 세로보다 길거나 같은 경우 
-            if(width >= i){
-                if((i-2) * (width -2) == yellow) {
-                    answer[0] = width;
-                    answer[1] = i;
+        for(int i=3; i<=carpet; i++){
+            
+            int col = i;
+            int row = carpet / i;
+            
+            if(row >= col){
+                if((row-2) * (col-2) == yellow){
+                    answer[0] = row;
+                    answer[1] = col;
                     break;
-                }     
+                }
             }
+            
+            
         }
+        
+        
         return answer;
     }
 }
