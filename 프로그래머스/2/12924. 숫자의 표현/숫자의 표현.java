95140ceb751086dc0a9751faa39dd567cf1cd360
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-25 20:59:46 +0900

    [level 2] Title: 숫자의 표현, Time: 1.71 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,22 +1,21 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int sum = 0;
         
-        for(int i=1; i<n;i++){
-            sum = i;
-            for(int j=i+1; j<=n; j++){
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+            for(int j=i; j<=n; j++){
                 sum += j;
+                
                 if(sum == n){
                     answer++;
-                    break;
-                }else if(sum > n){
+                }
+                
+                if(sum > n){
                     break;
                 }
             }
         }
-        
-        
-        return answer+1;
+        return answer;
     }
 }
