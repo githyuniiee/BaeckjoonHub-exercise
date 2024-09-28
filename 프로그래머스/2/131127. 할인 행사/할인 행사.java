0c9505d755de4a9daa3b4b8debeb561253bd0a1a
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2024-09-28 12:05:39 +0900

    [level 2] Title: 할인 행사, Time: 102.36 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,32 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int days = 10;
+        Map<String, Integer> targetMap = new HashMap<>();
         
-        Map<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<want.length; i++){
-            map.put(want[i], number[i]);
+        // 원하는 물품 목록을 map에 넣어줌
+        for (int i = 0; i < want.length; i++) {
+            targetMap.put(want[i], number[i]);
         }
-        
-        for(int i=0; i<= discount.length - days; i++){
-            Map<String, Integer> dMap = new HashMap<>();
-            
-            for(int j=0; j<days; j++){
-                dMap.put(discount[i+j], dMap.getOrDefault(discount[i+j],0) + 1);
+
+        // 10일 동안의 할인 품목을 확인하기 위한 슬라이딩 윈도우
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> currentMap = new HashMap<>();
+
+            // 현재 10일간의 할인 품목을 map에 넣어줌
+            for (int j = i; j < i + 10; j++) {
+                currentMap.put(discount[j], currentMap.getOrDefault(discount[j], 0) + 1);
             }
-            Boolean isIdentical = true;
-        
-        for(String key : map.keySet()){
-            if(map.get(key) != dMap.get(key)){
-                isIdentical = false;
-                break;
+
+            // 원하는 물품 목록과 현재 10일간의 할인 품목을 비교
+            boolean match = true;
+            for (String item : targetMap.keySet()) {
+                if (currentMap.get(item) != targetMap.get(item)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                answer++;
             }
         }
-          answer += isIdentical ? 1 : 0;
-        }
-     
-         return answer;
-        
+
+        return answer;
     }
 }
