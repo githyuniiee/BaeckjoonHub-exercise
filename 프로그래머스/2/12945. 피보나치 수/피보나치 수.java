dd3fe7091f2ed5dca01aca51cc9d3fdfb6f216b4
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-02 13:48:15 +0900

    [level 2] Title: 피보나치 수, Time: 6.07 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,21 +1,20 @@
 class Solution {
     public int solution(int n) {
-        
-        int answer = fibo(n);
-        
-        return answer;
+        return fibo(n);
     }
     
-    static int fibo(int num){
-        int[] cache = new int[num+1];
+    public int fibo(int num){
+        if(num == 0) return 0;
+        if(num == 1) return 1;
         
-        cache[0] = 0;
-        cache[1] = 1;
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
         
-        for(int i=2; i<=num; i++){
-            cache[i] = (cache[i-1] + cache[i-2]) % 1234567;
+        for(int i=2; i<= num; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
-        
-        return cache[num];
+        return dp[num];
     }
+
 }
