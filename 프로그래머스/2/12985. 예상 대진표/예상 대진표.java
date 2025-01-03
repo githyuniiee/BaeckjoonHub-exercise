99f3bcffae17e59fc1cdf79f09b27d7542e410a8
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-03 11:06:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,15 +2,17 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int round = 0;
-        while(true){
-            a = (a/2) + (a%2);
-            b = (b/2) + (b%2);
-            round++;
+        
+        int answer = 0;
+        
+        while( a != b ){
+            a = a / 2 + a % 2;
+            b = b / 2 + b % 2;
             
-            if(a == b) break;
+            answer++;
         }
         
-        return round;
+
+        return answer;
     }
 }
