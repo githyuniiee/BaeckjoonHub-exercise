ec27a87c5257102dcc0e9a80e9eade1c1d7cedc8
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-03 21:00:28 +0900

    [level 2] Title: 할인 행사, Time: 61.86 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,33 +3,46 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String, Integer> targetMap = new HashMap<>();
-        
-        // 원하는 물품 목록을 map에 넣어줌
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+        int num = 0;
+
+        // 원하는 상품과 수량을 map1에 저장
         for (int i = 0; i < want.length; i++) {
-            targetMap.put(want[i], number[i]);
+            map1.put(want[i], number[i]);
+            num += number[i];
         }
 
-        // 10일 동안의 할인 품목을 확인하기 위한 슬라이딩 윈도우
-        for (int i = 0; i <= discount.length - 10; i++) {
-            Map<String, Integer> currentMap = new HashMap<>();
+        // 초기 윈도우 설정
+        for (int i = 0; i < num; i++) {
+            map2.put(discount[i], map2.getOrDefault(discount[i], 0) + 1);
+        }
 
-            // 현재 10일간의 할인 품목을 map에 넣어줌
-            for (int j = i; j < i + 10; j++) {
-                currentMap.put(discount[j], currentMap.getOrDefault(discount[j], 0) + 1);
-            }
-
-            // 원하는 물품 목록과 현재 10일간의 할인 품목을 비교
-            boolean match = true;
-            for (String item : targetMap.keySet()) {
-                if (currentMap.get(item) != targetMap.get(item)) {
-                    match = false;
+        // 슬라이딩 윈도우로 비교
+        for (int i = num; i <= discount.length; i++) {
+            // map1과 map2를 비교
+            boolean isMatch = true;
+            for (String key : map1.keySet()) {
+                if (!map1.get(key).equals(map2.getOrDefault(key, 0))) {
+                    isMatch = false;
                     break;
                 }
             }
-
-            if (match) {
+            if (isMatch) {
                 answer++;
+            }
+
+            // 윈도우를 한 칸 오른쪽으로 이동
+            if (i < discount.length) {
+                String startItem = discount[i - num];
+                String endItem = discount[i];
+
+                map2.put(startItem, map2.get(startItem) - 1);
+                if (map2.get(startItem) == 0) {
+                    map2.remove(startItem);
+                }
+
+                map2.put(endItem, map2.getOrDefault(endItem, 0) + 1);
             }
         }
 
