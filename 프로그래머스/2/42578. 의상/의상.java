f56f7d50b9355a80afa23850b538fc608ddb4fe2
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-06 11:08:50 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-      
-        int result = 1;
-        HashMap<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        Map<String, Integer> hm = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++){
             String key = clothes[i][1];
-            map.put(key, map.getOrDefault(key, 0)+1);
+            hm.put(key, hm.getOrDefault(key,0) + 1);
         }
         
-        for(String key : map.keySet()){
-            result *= (map.get(key)+1); //안 입는 경우 고려해 +1
+        for(String key : hm.keySet()){
+            answer *= hm.get(key) +1;
         }
-        
-        result -= 1;
-        
-        return result;
-      
-        
+        return answer - 1;
     }
 }
