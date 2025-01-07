49c121bac396b3c7eade3c35977877d6d927a0b5
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-07 16:17:04 +0900

    [level 2] Title: 피로도, Time: 2.96 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,34 +1,28 @@
-import java.util.*;
-
 class Solution {
-    
-    static int[][] dungeon;
-    static boolean[] visited;
-    static int len;
-    static int answer;
-    
-    static void dfs(int remainingFatigue, int depth) {
-        answer = Math.max(answer, depth); // 최대 깊이(탐험 수)를 갱신
-
-        // 모든 던전을 순회하며 방문하지 않은 던전을 탐험
-        for (int i = 0; i < len; i++) {
-            if (!visited[i] && dungeon[i][0] <= remainingFatigue) {
-                visited[i] = true;
-                dfs(remainingFatigue - dungeon[i][1], depth + 1);
-                visited[i] = false; // 탐험 후 다시 초기화
-            }
-        }
-    }
+    public int answer;
+    public boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        dungeon = dungeons;
-        len = dungeons.length;
-        visited = new boolean[len];
-        answer = 0;
-
-        // DFS 탐색 시작
-        dfs(k, 0);
-
+        
+        visited = new boolean[dungeons.length];
+        
+        dfs(0, k, dungeons);
+        
         return answer;
     }
+ 
+    public void dfs(int depth, int k, int[][] dungeons){
+        
+        for(int i=0; i<dungeons.length; i++){
+            
+            if(!visited[i] && dungeons[i][0] <= k){
+                visited[i] = true;
+                dfs(depth + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+        
+        answer = Math.max(answer, depth);
+    }
+    
 }
