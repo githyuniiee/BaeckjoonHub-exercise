a47eaf86efd78c6a7feb8435dc1b9208a792092d
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-07 20:43:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.84 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.5 MB, 시간: 77.77 ms
+메모리: 96.6 MB, 시간: 76.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 14:34:37
+2025년 01월 07일 20:43:02
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,30 +1,26 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
-        String answer = "";
-        HashMap<String, Integer> hm = new HashMap<>();
-        
-        for(String player : participant) {
-            hm.put(player, hm.getOrDefault(player, 0) +1);
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 참가자 명단을 해시맵에 추가하며 카운트 증가
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        for(String player : completion) {
-            hm.put(player, hm.get(player) - 1);
+
+        // 완주자 명단에 따라 카운트 감소
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
         }
-        
-        Iterator<Map.Entry<String, Integer>> iter = hm.entrySet().iterator();
-        
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if(entry.getValue() != 0) {
-                answer = entry.getKey();
-                break;
+
+        // 값이 0이 아닌 사람(완주하지 못한 사람)을 찾기
+        for (String name : map.keySet()) {
+            if (map.get(name) != 0) {
+                return name;
             }
         }
-    
-        
-        return answer;
+
+        return ""; // 완주하지 못한 사람이 없는 경우 (문제 조건에서는 발생하지 않음)
     }
-    
 }
