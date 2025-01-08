64163c409b8570b4cb060a9595e75a6e35cd0c12
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-08 17:47:49 +0900

    [level 2] Title: 튜플, Time: 85.23 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,25 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
-        s = s.substring(2, s.length()-2);
-        String[] arr = s.split("\\},\\{");
-        Map<String, Integer> map = new HashMap<>();
         
-        for(String str : arr){
-            String[] numArr = str.split(",");
-            for(String num : numArr){
-                map.put(num, map.getOrDefault(num,0)+1); 
-            }
+        String[] strs = s.replaceAll("[{}]","").split(",");
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(String st : strs){
+            int num = Integer.parseInt(st);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        List<String> keyList = new ArrayList<>(map.keySet());
-        keyList.sort((s1,s2) -> map.get(s2) - map.get(s1));
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort((a,b) -> b.getValue() - a.getValue());
         
-        answer = new int[keyList.size()];
-        for(int i=0; i<answer.length; i++){
-            answer[i] = Integer.parseInt(keyList.get(i));
+        int[] answer = new int[entryList.size()];
+        for(int i=0; i<entryList.size(); i++){
+            answer[i] = entryList.get(i).getKey();
         }
+        
         return answer;
     }
 }
