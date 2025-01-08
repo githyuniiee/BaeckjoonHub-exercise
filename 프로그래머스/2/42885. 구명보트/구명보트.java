c412ff2464efcf29a036c334ecb6b7d468f210c5
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-08 09:46:00 +0900

    [level 2] Title: 구명보트, Time: 12.61 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,18 +1,23 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
         Arrays.sort(people);
-        int min = 0;
         
-        for(int max = people.length-1; min <= max; max--){
-            if(people[min] + people[max] <= limit) min++; {
+        int start = 0;
+        int end = people.length - 1;
+        
+        while(start <= end){
+            if(people[start] + people[end] > limit){
+                end--;
+                answer++;
+            
+            }else{
+                start++;
+                end--;
                 answer++;
             }
         }
-        
         
         return answer;
     }
