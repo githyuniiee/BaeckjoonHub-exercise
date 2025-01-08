210bb304942f91545bd850f7dabb61a56d2e0fb8
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-08 16:35:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.02 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,56 @@
 import java.util.*;
 
 class Solution {
+    static boolean[][] visited;
+    static int[] dy = new int[]{-1,0,0,1};
+    static int[] dx = new int[]{0,-1,1,0};
     
-    static int[] dx = new int[]{-1,0,0,1};
-    static int[] dy = new int[]{0,-1,1,0};
+    public int solution(int[][] maps) {
+        int answer = 0;
+        visited = new boolean[maps.length][maps[0].length];
+        
+        bfs(0,0, maps);
+        
+        if(maps[maps.length-1][maps[0].length-1] <=1){
+            answer = -1;
+        }else{
+            answer = maps[maps.length-1][maps[0].length-1];
+        }
+        
+        
+        return answer;
+    }
     
-    static void bfs(int[][] maps, int y, int x){
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(y,x));
-    
+    public void bfs(int y, int x, int[][] maps){
+        Queue<Loc> q = new LinkedList<>();
+        q.add(new Loc(y,x));
+        visited[y][x] =true;
         
         while(!q.isEmpty()){
-            Node node = q.poll();
+            Loc now = q.poll();
             
             for(int i=0; i<4; i++){
-                int next_x = node.x + dx[i];
-                int next_y = node.y + dy[i];
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
                 
-                if((0<= next_y && next_y <maps.length)
-                  && (0<= next_x && next_x<maps[0].length)
-                  && maps[next_y][next_x] == 1){
-                    q.add(new Node(next_y, next_x));
-                    maps[next_y][next_x] = maps[node.y][node.x] + 1;
+                if(0<=ny && ny < maps.length && 0<= nx && nx < maps[0].length
+                  && !visited[ny][nx] && maps[ny][nx] == 1){
+                    visited[ny][nx] = true;
+                    q.add(new Loc(ny, nx));
+                    maps[ny][nx] = maps[now.y][now.x] + 1;
                 }
             }
+            
         }
     }
-    public int solution(int[][] maps) {
-        
-        
-        bfs(maps,0,0);
-        
-        if(maps[maps.length - 1][maps[0].length -1] > 1 ){
-            return maps[maps.length - 1][maps[0].length -1];
-        }
-        return -1;
-    }
-}
-
-class Node {
-    int x;
-    int y;
     
-    public Node(int y, int x){
-        this.x = x;
-        this.y = y;
+    public class Loc{
+        int y;
+        int x;
+        
+        public Loc(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
     }
 }
