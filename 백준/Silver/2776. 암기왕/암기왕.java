1b2d3bbae201d39f8c2c8b010046925ff1b7486b
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-14 08:13:53 +0900

    [Silver IV] Title: 암기왕, Time: 1368 ms, Memory: 334260 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,35 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[] arr;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); // StringBuilder 사용
         int t = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < t; i++) {
-            HashSet<Integer> set = new HashSet<>();
-            int first = Integer.parseInt(br.readLine());
-            String str = br.readLine();
-            String[] st = str.split(" ");
 
-            for (int j = 0; j < st.length; j++) {
-                set.add(Integer.parseInt(st[j]));
+            sb = new StringBuilder();
+            int first = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr = new int[first];
+
+            for (int j = 0; j < first; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(arr);
 
             int second = Integer.parseInt(br.readLine());
-            StringTokenizer stk = new StringTokenizer(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < second; j++) {
-                int num = Integer.parseInt(stk.nextToken());
-                if (set.contains(num)) {
-                    sb.append(1).append("\n");
-                } else {
-                    sb.append(0).append("\n");
-                }
+                int num = Integer.parseInt(st.nextToken());
+                binarySearch(num);
+
+            }
+
+            System.out.print(sb);
+        }
+
+    }
+
+    public static void binarySearch(int num){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+
+            int middle = (start + end) / 2;
+            if(arr[middle] == num){
+                sb.append("1").append("\n");
+                return;
+            }
+
+            if(arr[middle] < num) {
+                start = middle + 1;
+            }else{
+                end = middle - 1;
             }
         }
-        System.out.print(sb); 
+
+        sb.append("0").append("\n");
     }
 }
