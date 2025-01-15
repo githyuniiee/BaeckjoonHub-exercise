2ef4d2d1d1f675ed1d788c449fe0f586c9fb1ed7
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-15 14:00:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 164 ms, Memory: 32888 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,82 +1,104 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
+import org.w3c.dom.Node;
 
 public class Main {
+
+    static ArrayList<Integer>[] list;
     static StringBuilder sb = new StringBuilder();
-    static int n,m,v;
-    static int[][] graph;
-    static boolean[] visited;
-    static Queue<Integer> q = new LinkedList<>();
 
-    static void dfs(int start){
-        visited[start] = true;
-        sb.append(start + " ");
+    public static void main(String[] args) throws Exception {
 
-        for(int i=1; i<=n; i++){
-            if( graph[start][i] == 1 && !visited[i]){
-               dfs(i);
-            }
-        }
-    }
-
-    static void bfs(int start) {
-
-        //시작할 노드 번호 넣기
-        q.offer(start);
-
-        //시작 노드 방문 처리
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            int nodeIndex = q.poll();
-            sb.append(nodeIndex + " ");
-
-            for (int i = 1; i <= n; i++) {
-
-                if (graph[nodeIndex][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    q.offer(i);
-                }
-            }
-        }
-    }
-
-
-
-    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 
-        graph = new int[n + 1][n + 1];
-        visited = new boolean[n + 1];
+        list = new ArrayList[n + 1];
 
+
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
 
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph[a][b] = 1;
-            graph[b][a] = 1;
+            list[a].add(b);
+            list[b].add(a);
         }
 
-        dfs(v);
-        sb.append("\n");
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(list[i]);
+        }
+
+
+        boolean[] visited = new boolean[n + 1];
+        String answer1 = dfs(v, visited);
+        System.out.println(answer1);
+
         visited = new boolean[n + 1];
+        String answer2 = bfs(v, visited);
+        System.out.println(answer2);
 
-        bfs(v);
-
-        System.out.println(sb);
     }
+
+    static String dfs(int start, boolean[] visited){
+
+        sb.append(start + " ");
+        visited[start] = true;
+
+        for(int i=0; i<list[start].size(); i++){
+            int next = list[start].get(i);
+
+            if(!visited[next]){
+                dfs(next, visited);
+            }
+
+        }
+
+        return sb.toString().trim();
+
+    }
+
+    static String bfs(int start, boolean[] visited) {
+
+        sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        sb.append(start + " ");
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            for (int i = 0; i < list[now].size(); i++) {
+                int next = list[now].get(i);
+
+                if (!visited[next]) {
+                    q.add(next);
+                    visited[next] = true;
+                    sb.append(next + " ");
+                }
+
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
 }
 
 
