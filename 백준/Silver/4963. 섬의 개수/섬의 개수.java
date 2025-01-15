8ab57cbdd4da35400412f2f8cb0cea49f2b7346e
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-15 15:30:45 +0900

    [Silver II] Title: 섬의 개수, Time: 108 ms, Memory: 13692 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,77 +1,101 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
+
 
 public class Main {
 
-
-    final static int MAX = 50 + 10;
-    static int map[][];
-    static boolean visited[][];
     static int w,h;
-    static int dirY[] = {-1, -1,-1, 0,0,1,1,1};
-    static int dirX[] = {-1, 0, 1, -1, 1,-1,0,1};
-    static int answer = 0;
-
-    public static void dfs(int y, int x){
-
-        visited[y][x] = true;
-        for(int i=0; i<8; i++){
-            int ny = y + dirY[i];
-            int nx = x + dirX[i];
-
-            if (!visited[ny][nx] && map[ny][nx] == 1) {
-
-                dfs(ny, nx);
-            }
+    static int[] dy = new int[]{-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dx = new int[]{-1, 0, 1, -1, 1, -1, 0, 1};
+    static int[][] arr;
+    static boolean[][] visited;
 
 
+    static class Node{
+        int y;
+        int x;
+
+        Node(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
-
     }
 
-    public static void main(String[] args) throws IOException {
+
+
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
+        while(true){
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
 
-            if(w == 0 && h == 0) break;
+            if(w == 0 && h == 0){
+                break;
+            }
 
-            map = new int[MAX][MAX];
-            visited = new boolean[MAX][MAX];
+            arr = new int[h][w];
+            visited = new boolean[h][w];
 
-            for (int i = 1; i <= h; i++) {
+            for (int i = 0; i < h; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 1; j <= w; j++) {
-                    map[j][i] = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < w; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            //dfs
-            for (int i = 1; i <= h; i++) {
-
-                for (int j = 1; j <= w; j++) {
-                    if (map[j][i] == 1 && !visited[j][i]) {
-                        dfs(j, i);
-                        answer++;
+            int count = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && arr[i][j] == 1) {
+                        bfs(i,j);
+                        count++;
                     }
                 }
             }
 
-            System.out.println(answer);
-            answer = 0;
+            System.out.println(count);
+        }
 
 
+
+
+    }
+
+    static void bfs(int y, int x){
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(y, x));
+        visited[y][x] = true;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int ny = node.y + dy[i];
+                int nx = node.x + dx[i];
+                
+
+                if(0<=ny && ny < h && 0<=nx && nx < w
+                && !visited[ny][nx] && arr[ny][nx] == 1){
+                    q.add(new Node(ny, nx));
+                    visited[ny][nx] = true;
+                }
+            }
         }
     }
 
 
 
+
 }
+
+
