00a83f4e791f6624171ffe8720955bda69721c0e
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-17 10:27:21 +0900

    [Silver I] Title: 기타 레슨, Time: 216 ms, Memory: 22444 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,57 +1,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
 
 public class Main {
 
-    static int n;
-    static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
 
-        int start = 0;
-        int end = 0;
-        int result = 0;
+        int[] lesson = new int[n];
+        int left = 0, right = 0, answer = 0;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            if (start < arr[i]) {
-                start = arr[i];
-            }
-            end += arr[i];
+            lesson[i] = Integer.parseInt(st.nextToken());
+            left = Math.max(left, lesson[i]);
+            right += lesson[i];
+            answer += lesson[i];
         }
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            int count = 0;
-            int sum = 0;
+        while(left <= right){
+            int mid = (left + right ) / 2;
+            int count = 1;
+            int sum = lesson[0];
 
-            for (int i = 0; i < n; i++) {
-
-                if(sum + arr[i] > mid){
+            for (int i = 1; i < n; i++) {
+                if(sum + lesson[i] > mid){
                     count++;
-                    sum = 0;
+                    sum = lesson[i];
+                }else{
+                    sum += lesson[i];
                 }
-                sum += arr[i];
             }
 
-            count++;
-
-            if(count <= m){
-                result = mid;
-                end = mid - 1;
+            if(count > m){
+                left = mid + 1;
             }else{
-                start = mid + 1;
+                right = mid -1;
+                answer = Math.min(answer, mid);
             }
         }
 
-        System.out.println(result);
+
+        System.out.println(answer);
+
+
+
+
+
 
 
 
@@ -60,3 +62,5 @@ public class Main {
 
 
 }
+
+
