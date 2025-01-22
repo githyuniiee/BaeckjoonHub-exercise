53c9f04d3ea51e6b8342c725117f7b32afdf56df
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-01-22 16:00:49 +0900

    [Silver V] Title: 문서 검색, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,35 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         String find = br.readLine();
-        int find_len = find.length();
-        int count = 0;
+        int len = find.length();
 
         int start = 0;
-        while (true) {
-            if(start+find_len > str.length()) break;
-            String a = str.substring(start, start + find_len);
-            if(a.equals(find)){
-                count++;
-                start = start + find_len;
+        int end = len;
+        int answer = 0;
+
+        while(end <= str.length()){
+
+            String search = str.substring(start, end);
+
+            if(search.equals(find)){
+                answer++;
+                start = end;
+                end += len;
             }else{
-                start++;
+                start += 1;
+                end += 1;
             }
         }
 
-        System.out.println(count);
-
+        System.out.println(answer);
 
     }
+
 
 }
