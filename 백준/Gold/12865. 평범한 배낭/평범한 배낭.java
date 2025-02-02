1a74f4ec4b2dd5c09dd71a525a8eed5321bfcc3b
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-02 10:47:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 51400 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,16 +6,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+
     public static void main(String[] args)throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
         int[] weight = new int[n+1];
-        int[] value = new int[k+1];
+        int[] value = new int[n+1];
         int[][] dp = new int[n+1][k+1];
 
         for(int i=1; i<=n; i++){
@@ -25,16 +25,19 @@ public class Main {
 
             weight[i] = w;
             value[i] = v;
+
         }
 
         for(int i=1; i<=n; i++){
+
             for(int j=1; j<=k; j++){
 
                 //담을 수 X
                 if(weight[i] > j){
                     dp[i][j] = dp[i-1][j];
                 }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - weight[i]] + value[i]);
+                    //담을 수 O
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
                 }
             }
         }
@@ -42,7 +45,9 @@ public class Main {
         System.out.println(dp[n][k]);
 
 
-    
+
+
         
+
     }
 }
