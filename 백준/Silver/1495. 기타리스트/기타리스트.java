a661505c8f79e0d7f71a3f2f9f3b292d2a5efd9f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-03 21:34:51 +0900

    [Silver I] Title: 기타리스트, Time: 68 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -5,52 +5,56 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, s, m;
-    static int[] change;
-    static boolean[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
-        change = new int[n];
-        dp = new boolean[n+1][m+1];
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        boolean[][] list = new boolean[n + 1][m + 1]; 
+        list[0][s] = true; 
+        int answer = -1;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            change[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0][s] = true;
+        for (int i = 0; i < n; i++) {
+            boolean pass = false;
+            for (int j = 0; j <= m; j++) {
+                if (list[i][j]) {  
+                    int first = arr[i] + j;
+                    int second = j - arr[i];
 
-        for(int i=0; i<n; i++){
-            for(int v = 0; v<=m; v++){
-                if(dp[i][v]){
-                    if (v + change[i] <= m) {  
-                        dp[i + 1][v + change[i]] = true;
+                    if (0 <= first && first <= m) {
+                        list[i + 1][first] = true;
+                        pass = true;
                     }
-                    if (v - change[i] >= 0) {  
-                        dp[i + 1][v - change[i]] = true;
+
+                    if (0 <= second && second <= m) {
+                        list[i + 1][second] = true;
+                        pass = true;
                     }
                 }
             }
+
+
+            if (!pass) break;
         }
 
-        int answer = -1;
-        for(int v = m; v>=0; v--){
-            if(dp[n][v]){
-                answer = v;
+
+        for (int i = m; i >= 0; i--) {
+            if (list[n][i]) {
+                answer = i; 
                 break;
             }
         }
 
+
         System.out.println(answer);
-
-        
     }
-
-   
 }
