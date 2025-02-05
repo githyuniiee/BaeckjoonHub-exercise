90c35e07bd31c5e53413a056df1290168db19545
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-05 21:07:38 +0900

    [Silver I] Title: 부등호, Time: 124 ms, Memory: 23120 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,72 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int N;
-    private static char[] arr = new char[10]; //부등호 최대 9개
-    private static boolean[] visited = new boolean[10]; //숫자 0~9
-    private static List<String> ans = new ArrayList<>();
+	static int k;
+	static char arr[];
+	static boolean visit[] = new boolean[10];
+	static ArrayList<String> result = new ArrayList<>();
 
-    static boolean ck(char a, char b, char c) {
-        if (c == '<') {
-            if (a > b) {
-                return false;
-            }
-        }
-        if (c == '>') {
-            if (a < b) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    static void go(int index, String num) {
-        if (index == N + 1) {
-            ans.add(num);
-            return;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if(visited[i])
-                continue;
-            if(index == 0 || ck(num.charAt(index-1), (char) (i + '0'), arr[index-1] )) {
-                visited[i] = true;
-                go(index + 1, num + Integer.toString(i));
-                visited[i] = false;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		k = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new char[k];
 
-        N = Integer.parseInt(br.readLine());
+		for(int i=0; i<k; i++){
+			arr[i] = st.nextToken().charAt(0);
+		}
 
+		for (int i = 0; i < 10; i++) {
+		
+			visit[i] = true;
+			dfs(i,0, i+"");
+			visit[i] = false;
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = st.nextToken().charAt(0);
-        }
+		Collections.sort(result);
+		System.out.println(result.get(result.size()-1));
+		System.out.println(result.get(0));
+	
 
-        go(0, "");
-        Collections.sort(ans);
+	
+	}
 
-        System.out.println(ans.get(ans.size() - 1));
-        System.out.println(ans.get(0));
+	static void dfs(int now, int count, String number){
+		if(count == k){
+			result.add(number);
+			return;
+		}
 
-
-
-
-
-    }
-
+		for(int next=0; next<10; next++){
+			if(!visit[next]){
+				if((arr[count] == '<'&& now < next) || (arr[count] == '>' && now > next)){
+					visit[next] = true;
+					dfs(next, count+1, number + next);
+					visit[next] = false;
+				}
+			}
+		}
+	}
 }
+
