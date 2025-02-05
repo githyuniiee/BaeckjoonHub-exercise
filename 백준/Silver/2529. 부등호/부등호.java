e41a57e7b1563496bb5cd1b09078fb94ff242041
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-05 21:24:02 +0900

    [Silver I] Title: 부등호, Time: 124 ms, Memory: 21524 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,55 +1,59 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static int k;
-	static char arr[];
-	static boolean visit[] = new boolean[10];
-	static ArrayList<String> result = new ArrayList<>();
+	static int n;
+	static char[] arr;
+	static ArrayList<String> list = new ArrayList<>();
+	static boolean[] visit;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		k = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new char[k];
+		n = Integer.parseInt(br.readLine());
 
-		for(int i=0; i<k; i++){
+		arr = new char[n];
+		visit = new boolean[10];
+	
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for(int i=0; i<n; i++){
 			arr[i] = st.nextToken().charAt(0);
 		}
 
 		for (int i = 0; i < 10; i++) {
-		
+
 			visit[i] = true;
-			dfs(i,0, i+"");
+			dfs(i, 0, i+"");
 			visit[i] = false;
 		}
 
-		Collections.sort(result);
-		System.out.println(result.get(result.size()-1));
-		System.out.println(result.get(0));
-	
+		Collections.sort(list);
 
-	
+		System.out.println(list.get(list.size() - 1));
+		System.out.println(list.get(0));
+
+
 	}
 
-	static void dfs(int now, int count, String number){
-		if(count == k){
-			result.add(number);
+	static void dfs(int now, int count, String str){
+		if(count == n){
+			list.add(str);
 			return;
 		}
 
-		for(int next=0; next<10; next++){
-			if(!visit[next]){
-				if((arr[count] == '<'&& now < next) || (arr[count] == '>' && now > next)){
-					visit[next] = true;
-					dfs(next, count+1, number + next);
-					visit[next] = false;
+
+		for(int i=0; i<10; i++){
+			if(!visit[i]){
+				if((arr[count] == '<' && now < i) || (arr[count] == '>' && now > i)){
+					visit[i] = true;
+					dfs(i, count + 1, str + i);
+					visit[i] = false;
 				}
 			}
 		}
 	}
+
 }
 
