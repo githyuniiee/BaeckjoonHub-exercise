150fffbc67d0b00e683770610be3c5d8e9c8ef92
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-06 09:43:16 +0900

    [Silver II] Title: 스택 수열, Time: 268 ms, Memory: 28372 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,65 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int myArr[];
-    static int checkArr[];
-    static int checkSecret;
 
-    public static void main(String[] args)throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        int a = 1;
-        ArrayList<String> list = new ArrayList<>();
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<n; i++){
+		Stack<Integer> stack = new Stack<>();
 
-            int num = Integer.parseInt(br.readLine());
+		int now = 1;
+		for(int i=0; i<n; i++){
+			int num = Integer.parseInt(br.readLine());
 
-            if(stack.isEmpty()){
-                while(a <= num){
-                    stack.push(a);
-//                    System.out.println("+");
-                    list.add("+");
-                    a++;
-                }
-            }
-
-            if(num == stack.peek()){
-                stack.pop();
-//                System.out.println("-");
-                list.add("-");
-
-            }else{
-                if(a<=num) {
-                    while (a <= num) {
-                        stack.push(a);
-//                        System.out.println("+");
-                        list.add("+");
-                        a++;
-                    }
-                    stack.pop();
-//                    System.out.println("-");
-                    list.add("-");
-                }else{
-                    list.clear();
-                   list.add("NO");
-                    break;
-                }
-            }
+			if(stack.isEmpty()){
+				for(int j=now; j<=num; j++){
+					sb.append("+" + "\n");
+					stack.push(j);
+				}
+				now = num + 1;
+			}
 
 
-        }
+			if(stack.peek() == num){
+				sb.append("-" + "\n");
+				stack.pop();
+			}else{
+				if(now <= num){
+					while(now <= num){
+						stack.push(now);
+						sb.append("+" + "\n");
+						now++;
+					}
+					stack.pop();
+					sb.append("-" + "\n");
+				}else{
+					sb.setLength(0);
+					sb.append("NO");
+					break;
+				}
+			}
+			
+		}
 
-        for(int i=0; i<list.size(); i++){
-            System.out.println(list.get(i));
-        }
+		System.out.println(sb);
 
-
+		
     }
+
 }
+
