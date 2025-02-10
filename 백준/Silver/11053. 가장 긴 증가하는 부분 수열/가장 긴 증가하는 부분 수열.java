a83570f71440b862aff7217bc290b534fdc894d4
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-11 08:49:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,43 +1,39 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
-
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        int[] answer = new int[n+1];
-        int result = 0;
 
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        int answer = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i=1; i<=n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i=1; i<=n; i++){
-            answer[i] = 1;
-            for(int j=1; j<i; j++){
+            dp[i] = 1;
+            for(int j=0; j<i; j++){
                 if(arr[j] < arr[i]){
-                    answer[i] = Math.max(answer[i],answer[j] + 1);
-//                    System.out.println(answer[i]);
+                dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            result = Math.max(result, answer[i]);
+            answer = Math.max(dp[i], answer);
         }
 
-        System.out.println(result);
+        System.out.println(answer);
 
+        
 
     }
-
 }
-
-
