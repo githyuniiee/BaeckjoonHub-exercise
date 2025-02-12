34b9ad7ce74e661b29f2875de2279a85dbb813bc
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-12 09:00:29 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11836 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,34 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int result = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        //누적 시간
-        for (int i = 1; i < n; i++) {
-            arr[i] += arr[i - 1];
+        
+        dp[0] = arr[0];
+        int sum = dp[0];
+
+        for(int i=1; i<n; i++){
+            dp[i] = dp[i-1] + arr[i];
+            sum += dp[i];
         }
 
-        for (int j = 0; j < n; j++) {
-            result += arr[j];
-        }
+        System.out.println(sum);
 
-        System.out.println(result);
-    }
+    
+
+
+	}
+
+	
 }
