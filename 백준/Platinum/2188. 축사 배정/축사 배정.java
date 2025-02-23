6aee9180edaa1e8f3b25ddbd3e05952058316178
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-23 09:10:08 +0900

    [Platinum IV] Title: 축사 배정, Time: 136 ms, Memory: 14724 KB -BaekjoonHub

--- 백준/Platinum/2188. 축사 배정/축사 배정.java
+++ 백준/Platinum/2188. 축사 배정/축사 배정.java
@@ -1,52 +1,63 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static List<Integer>[] list;
-	static boolean[] check;
-	static int[] d;
-	public static void main(String[] args) throws  IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[n+1];
-		for(int i=1; i<=n; i++) {
+    static int n,m;
+   static List<Integer>[] list;
+    static int[] matched;
+    static boolean[] check;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[n+1];
+        matched = new int[m+1];
+        check = new boolean[m+1];
+
+        for(int i=1; i<=n; i++) {
 			list[i] = new ArrayList<>();
 		}
-		
-		check = new boolean[m+1];
-		d = new int[m+1];
-		
-		for(int i=1; i<=n; i++) {
+
+        for(int i=1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			for(int j=0; j<s; j++) {
 				list[i].add(Integer.parseInt(st.nextToken()));	
 			}
 		}
-		
-		int cnt = 0;
-		for(int i=1; i<=n; i++) {
-			Arrays.fill(check, false);
-			if(dfs(i)) cnt++;
-		}
-		System.out.println(cnt);
-	}
-	
-	static boolean dfs(int here) {
-		for(int nxt : list[here]) {
-			if(!check[nxt]) {
-				check[nxt] = true;
-				if(d[nxt] == 0 || dfs(d[nxt])) {
-					d[nxt] = here;
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+
+        
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            Arrays.fill(check, false);
+            if(dfs(i)) cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+
+    static boolean dfs(int cow){
+        
+        for(int num : list[cow]){
+            if(!check[num]){
+                check[num] = true;
+
+                if(matched[num] == 0 || dfs(matched[num])){
+                    matched[num] = cow;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
