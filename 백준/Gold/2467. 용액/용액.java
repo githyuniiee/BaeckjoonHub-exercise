2f453883bcc1119a607a7e37bfef5aeb43f374f7
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-02-28 21:15:32 +0900

    [Gold V] Title: 용액, Time: 244 ms, Memory: 32024 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,45 +1,42 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int left = 0;
-        int right = n-1;
-        int min = Integer.MAX_VALUE;
+        int right = n - 1;
+        int ans = Integer.MAX_VALUE;
         int n1 = 0;
         int n2 = 0;
 
         while(left < right){
 
-            if(Math.abs(arr[left] + arr[right]) < min){
+            if(Math.abs(arr[left] + arr[right]) < ans ){
+                ans = Math.abs(arr[left] + arr[right]);
                 n1 = arr[left];
                 n2 = arr[right];
-
-                min = Math.abs(arr[left] + arr[right]);
             }
 
-            if(arr[left] + arr[right] < 0){
-                left++;
-
-            }else if(arr[left] + arr[right] > 0){
-                right--;
-
-            }else{
+            if(arr[left] + arr[right] == 0){
                 break;
+            }else if(arr[left] + arr[right] < 0){
+                left++;
+            }else{
+                right--;
             }
         }
 
-        System.out.println(n1 +  " " + n2);
-
+        System.out.println(n1 + " " + n2);
         
         
 
