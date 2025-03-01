5cb1f4cf1123930c83a69b726ca26a9dcb09c388
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-01 10:57:40 +0900

    [Bronze II] Title: 벌집, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,28 +1,30 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
 
-
+    static String[] channel;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[1000000];
-        arr[0] = 1;
+        int ans = 1;
+        int now = 1;
 
+        while(true){
 
-        for (int i = 0; i < arr.length -1 ; i++) {
-            arr[i+1] = arr[i] + (6*(i+1));
-        }
-
-        for (int i = 0; i < arr.length -1 ; i++) {
-            if(num <= arr[i]){
-                System.out.println(i+1);
+            if(n <= now){
                 break;
             }
+
+            now += (6*ans);
+
+            ans++;
+
         }
 
+        System.out.println(ans);
 
+
+        
     }
 }
