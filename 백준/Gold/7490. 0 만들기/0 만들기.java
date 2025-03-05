55e853e5a5cfd654463bbf98e14bc3c96ab86b3b
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-05 22:54:59 +0900

    [Gold V] Title: 0 만들기, Time: 76 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,41 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
+    static int n;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            dfs(n, 1, 1, 0, "1");
+        for(int i=0; i<t; i++){
+            n = Integer.parseInt(br.readLine());
+
+            dfs(1,1,0,1,"1");
             sb.append("\n");
+            
         }
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
+
     }
 
-    static void dfs(int max, int now, int num, int sum, String result) {
-        if (max == now) {
-            sum += num; // 마지막 숫자 처리
-            if (sum == 0) {
-                sb.append(result).append("\n");
+    static void dfs(int idx, int num, int sum, int op, String str){
+
+   
+
+        if(idx == n){
+            sum += (num * op);
+            if(sum == 0){
+                sb.append(str).append("\n");
             }
             return;
         }
 
-        int next = now + 1;
+        int next = idx + 1;
 
-        // 공백
-        dfs(max, next, num * 10 + (num > 0 ? next : -next), sum, result + " " + next);
+         //공백
+        dfs(next, (num*10) + next,sum, op, str + " " + next);
 
-        // 덧셈
-        dfs(max, next, next, sum + num, result + "+" + next);
+        //더하기
+        dfs(next, next, sum + (num*op), 1, str + "+" + next);
+        
+        //빼기
+        dfs(next, next, sum + (num*op), -1, str + "-" + next);
 
-        // 뺄셈
-        dfs(max, next, -next, sum + num, result + "-" + next);
+        
+
     }
+
 }
