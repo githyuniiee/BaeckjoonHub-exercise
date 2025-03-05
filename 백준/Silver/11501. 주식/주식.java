fa0a37cfea8a4aa98a97cfcf82c839f2ecc47184
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-05 10:18:09 +0900

    [Silver II] Title: 주식, Time: 1056 ms, Memory: 323612 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,41 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
- 
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	public static int T, N;
-	public static int[] arr;
-	public static long answer = 0;
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	T = Integer.parseInt(st.nextToken());
-    	
-    	for(int i = 0;i < T; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		N = Integer.parseInt(st.nextToken());
-    		answer = 0;
-    		arr = new int[N];
-    		st = new StringTokenizer(br.readLine());
-    		for(int j=0;j<N;j++) {
-    			arr[j] = Integer.parseInt(st.nextToken()); 
-    		}
-    		
-    		long maxValue = 0;
-    		for(int j=N-1; j>=0;j--) {
-    			if(maxValue < arr[j]) {
-    				maxValue = arr[j];
-    			}else {
-    				answer += maxValue - arr[j];
-    			}
-    		}
-    		
-    		System.out.println(answer);
-    	}	
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<t; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[n];
+
+            for(int j=0; j<n; j++){
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            
+
+            long max = 0;
+            long ans = 0;
+
+            for(int j=n-1; j>=0; j--){
+                if(arr[j] < max){
+                    ans += max - arr[j];
+                }else{
+                    max = arr[j];
+                }
+            }
+
+            sb.append(ans).append("\n");
+        }
+
+
+        System.out.println(sb);
+     
+
     }
-    
 }
