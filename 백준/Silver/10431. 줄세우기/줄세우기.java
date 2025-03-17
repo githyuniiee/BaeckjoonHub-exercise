209e08f3c2c0f185cfe0cd10147a5b8f2dc2b935
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-17 09:23:37 +0900

    [Silver V] Title: 줄세우기, Time: 152 ms, Memory: 16636 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,48 +7,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-		Stack<Integer> stack2 = new Stack<>();
-		StringBuilder sb = new StringBuilder();
 		
 
-		
-
-		for(int i=0; i<n; i++){
+		for(int i=1; i<n+1; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int c = Integer.parseInt(st.nextToken());
-			int ans = 0;
+			Integer.parseInt(st.nextToken());
+
+			int cnt = 0;
+			int[] num = new int[20];
 
 			for(int j=0; j<20; j++){
-
-				int num = Integer.parseInt(st.nextToken());
-
-				if(stack.isEmpty()){
-					stack.push(num);
-				}else{
-					if(stack.peek() < num){
-						stack.push(num);
-					}else{
-						while(!stack.isEmpty() && stack.peek() > num){
-							stack2.push(stack.pop());
-							ans++;
-						}
-
-						stack.push(num);
-
-						while(!stack2.isEmpty()){
-							stack.push(stack2.pop());
-						}
-					}
-				}
-				
+				num[j] = Integer.parseInt(st.nextToken());
 			}
-			sb.append(c + " " + ans).append("\n");
-			stack.clear();
-			stack2.clear();
-		}
 
-		System.out.println(sb);
+			for(int j=0; j<20; j++){
+				for(int k=0; k<j; k++){
+					if(num[k]> num[j]) cnt++;
+				}
+			}
+			
+			System.out.println(i + " " + cnt);
+
+		}
 
 		
 	}
