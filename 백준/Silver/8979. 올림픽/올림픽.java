c71494a3a15558b7004b41071b3e635ecce1ad52
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-19 20:51:05 +0900

    [Silver V] Title: 올림픽, Time: 188 ms, Memory: 18192 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static class Nation {
+		int num;
+		int g;
+		int s;
+		int m;
+
+		Nation(int num,int g ,int s, int m){
+			this.num = num;
+			this.g = g;
+			this.s = s;
+			this.m = m;
+
+		}
+	}
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		PriorityQueue<Nation> pq = new PriorityQueue<>((o1,o2) -> {
+
+			if(o1.g == o2.g){
+				if(o1.s == o2.s){
+					return o2.m - o1.m;
+				}
+				return o2.s - o1.s;
+
+			}
+			return o2.g - o1.g;
+		});
+
+		for(int i=1; i<=n; i++){
+			st = new StringTokenizer(br.readLine());
+
+			int number = Integer.parseInt(st.nextToken());
+			int gold = Integer.parseInt(st.nextToken());
+			int silver = Integer.parseInt(st.nextToken());
+			int metal = Integer.parseInt(st.nextToken());
+
+			Nation nation = new Nation(number, gold, silver, metal);
+			pq.add(nation);
+		}
+
+		int idx = 1;
+
+		Nation now = pq.poll();
+		if(now.num == k){
+			System.out.println(idx);
+			return;
+		}
+
+		while(!pq.isEmpty()){
+
+			Nation next = pq.poll();
+
+			if(now.g==next.g && now.s == next.s && now.m == next.m){
+				now = next;
+
+				if(k == now.num){
+					System.out.println(idx);
+					return;
+				}
+				continue;
+			}
+
+			idx++;
+			now = next;
+
+			if(k == now.num){
+				System.out.println(idx);
+				break;
+			}
+		}
+
+
+
+
+
+
+
+    }
+}
