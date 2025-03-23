f797f4e343a9b5bfbb716831f31a44109dee150c
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-23 10:14:23 +0900

    [Gold V] Title: A와 B 2, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -3,42 +3,36 @@ import java.io.*;
 public class Main {
 
     static String s, t;
-    static boolean answer = false;
+    static int answer = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine();
         t = br.readLine();
 
-        dfs(new StringBuilder(t));
+		dfs(t);
+		System.out.println(answer);
 
-        System.out.println(answer ? 1 : 0);
     }
 
-    static void dfs(StringBuilder sb) {
-        // 종료 조건: 목표 문자열보다 길이가 짧아지면 탐색 중단
-        if (sb.length() < s.length()) return;
-        
-        // 목표 문자열과 같으면 성공
-        if (sb.toString().equals(s)) {
-            answer = true;
-            return;
-        }
+	static void dfs(String t){
 
-        // A 제거
-        if (sb.charAt(sb.length() - 1) == 'A') {
-            sb.deleteCharAt(sb.length() - 1);
-            dfs(sb);
-            sb.append('A');  // 백트래킹 (원상 복구)
-        }
+		int lenT = t.length();
 
-        // B 제거 후 뒤집기
-        if (sb.charAt(0) == 'B') {
-            sb.deleteCharAt(0);
-            sb.reverse();
-            dfs(sb);
-            sb.reverse();
-            sb.insert(0, 'B');  // 백트래킹 (원상 복구)
-        }
-    }
+		if(lenT == s.length()){
+			if(t.equals(s)){
+				answer = 1;
+			}
+			return;
+		}
+
+		if(t.endsWith("A")){
+			dfs(t.substring(0, lenT - 1));
+		}
+
+		if(t.startsWith("B")){
+			dfs(new StringBuilder(t.substring(1)).reverse().toString());
+		}
+	}
+
 }
