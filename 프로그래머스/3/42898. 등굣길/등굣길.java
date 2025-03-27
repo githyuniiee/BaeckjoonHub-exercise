19043dca59517e1d59b7bc145ec20fe8291a1c82
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-27 11:53:00 +0900

    [level 3] Title: 등굣길, Time: 0.92 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,26 @@
+import java.util.*;
+ 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int[][] dp = new int[n+1][m+1];
         int mod = 1000000007;
         
-       for(int[] temp: puddles){
-           dp[temp[1]][temp[0]] = -1;
-       }
+        int[][] arr = new int[m+1][n+1];
         
-        dp[1][1] = 1;
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if(dp[i][j] == -1){
-                    continue;
-                }
-                if(dp[i-1][j] > 0){
-                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
-                }
-                if(dp[i][j-1] > 0){
-                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
-                }
+        //웅덩이 초기화
+        for(int i=0; i<puddles.length; i++){
+            arr[puddles[i][0]][puddles[i][1]] = -1;
+        }
+        
+        //시작 지점
+        arr[1][1] = 1;
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(arr[i][j] == -1) continue;
+                if(arr[i-1][j] > 0) arr[i][j] += arr[i-1][j] % mod;
+                if(arr[i][j-1] > 0) arr[i][j] += arr[i][j-1] % mod;
             }
         }
-        return dp[n][m];
+        
+        return arr[m][n] % mod;
     }
 }
