d0136df5210829e0b408d906b50bbae0a18e5024
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-03-27 14:21:27 +0900

    [level 3] Title: N으로 표현, Time: 79.15 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,45 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if(N == number){
-            return 1;
-        }
+        if (N == number) return 1; // N이 number와 같다면 1 반환
         
-        //가능한 숫자 집합 담을 리스트
+        // DP 테이블 초기화 (1~8개의 N 사용 경우 저장)
         List<HashSet<Integer>> list = new ArrayList<>();
-        for(int i=0; i<=8; i++){
-            list.add(new HashSet<Integer>());
+        for (int i = 0; i <= 8; i++) {
+            list.add(new HashSet<>());
         }
-        
-        list.get(1).add(N); //숫자 1개만 쓸 때 -> 자기 자신
-        
-        //숫자 2개 쓸 때부터 탐색
-        for(int i=2; i<=8; i++){
+
+        // 1개 사용했을 때는 N 자체만 가능
+        list.get(1).add(N);
+
+        // DP 테이블 채우기
+        for (int i = 2; i <= 8; i++) {
+            HashSet<Integer> currentSet = list.get(i);
             
-            //숫자 i개 썼을 때 들어갈 통
-            HashSet<Integer> total = list.get(i);
+            // N을 i개 이어붙인 숫자 추가 (ex: 5, 55, 555, 5555)
+            int num = Integer.parseInt(String.valueOf(N).repeat(i));
+            currentSet.add(num);
             
-            //이전 통들을 통한 경우의 수 찾기    
-            for(int j=1; j<i; j++){
-                HashSet<Integer> a = list.get(j);
-                HashSet<Integer> b = list.get(i-j);
-                
-                for(int x : a){
-                    for(int y : b){
-                        total.add(x+y);
-                        total.add(x-y);
-                        total.add(x*y);
-                        if(x!=0 && y!=0){
-                            total.add(x/y);
-                        }
+            // j개 사용한 경우 + (i-j)개 사용한 경우 조합
+            for (int j = 1; j < i; j++) {
+                for (int num1 : list.get(j)) {
+                    for (int num2 : list.get(i - j)) {
+                        currentSet.add(num1 + num2);
+                        currentSet.add(num1 - num2);
+                        currentSet.add(num1 * num2);
+                        if (num2 != 0) currentSet.add(num1 / num2);
                     }
                 }
-                //같은숫자로 된 것 추가
-                total.add(Integer.parseInt(String.valueOf(N).repeat(i)));
             }
-            if(total.contains(number)) return i;
+
+            // 원하는 숫자가 있으면 정답 반환
+            if (currentSet.contains(number)) {
+                return i;
+            }
         }
-        
-        return -1;
+
+        return -1; // 8개 이하로 만들 수 없는 경우
     }
 }
