245bdbcaec64ac9f2aa19b41c947924676963ab4
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-05 10:44:53 +0900

    [level 2] Title: 피로도, Time: 3.28 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,28 +2,33 @@ import java.util.*;
 
 class Solution {
     
+    static boolean[] visited;
     static int len;
     static int answer = 0;
-    static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
+
         len = dungeons.length;
         visited = new boolean[len];
         
         dfs(k, dungeons, 0);
         
+        
         return answer;
     }
     
-    static void dfs(int k, int[][] dungeons, int cnt) {
-        answer = Math.max(answer, cnt); // 최대 탐험 횟수 갱신
-
-        for (int i = 0; i < len; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true; // 방문 처리
-                dfs(k - dungeons[i][1], dungeons, cnt + 1); // 다음 탐색
-                visited[i] = false; // 백트래킹 (복구)
+    static void dfs(int k, int[][] dungeons, int depth){
+        
+        answer = Math.max(answer, depth);
+        
+        for(int i=0; i<len; i++){
+            
+            if(k >= dungeons[i][0] && !visited[i]){
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, depth + 1);
+                visited[i] = false;
             }
         }
+        
     }
 }
