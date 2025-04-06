77f1f368276267c982075e1770c1aec6f6e4867d
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-06 09:34:09 +0900

    [Gold V] Title: 용액, Time: 232 ms, Memory: 31440 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,46 +1,53 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
 
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int left = 0;
-        int right = n - 1;
-        int ans = Integer.MAX_VALUE;
-        int n1 = 0;
-        int n2 = 0;
+		for(int i=0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        while(left < right){
-
-            if(Math.abs(arr[left] + arr[right]) < ans ){
-                ans = Math.abs(arr[left] + arr[right]);
-                n1 = arr[left];
-                n2 = arr[right];
-            }
-
-            if(arr[left] + arr[right] == 0){
-                break;
-            }else if(arr[left] + arr[right] < 0){
-                left++;
-            }else{
-                right--;
-            }
-        }
-
-        System.out.println(n1 + " " + n2);
-        
-        
+		int s = 0;
+		int e = n-1;
+		int ans1 = 0;
+		int ans2 = 0;
+		int min = Integer.MAX_VALUE;
 
 
-   
+		while(s < e){
+
+			if(min > Math.abs(arr[e]+arr[s])){
+				min = Math.abs(arr[e]+arr[s]);
+				ans1 = arr[s];
+				ans2 = arr[e];
+			}
+
+			if(arr[e] + arr[s] < 0){
+				s++;
+			}else if(arr[e] + arr[s] > 0){
+				e--;
+			}else{
+				ans1 = arr[s];
+				ans2 = arr[e];
+				break;
+			}
+		}
+
+		System.out.println(ans1 + " " + ans2);
+
+
+
+	
+		
     }
+
 }
