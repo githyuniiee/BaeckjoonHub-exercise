0d1713b495236f75f356e924cc22669d50b62b42
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-16 09:07:02 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,41 +1,63 @@
+import java.util.*;
+
 class Solution {
     
-    boolean[] visited;
-    int answer = 0;
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
         
         visited = new boolean[words.length];
+        
+        boolean containsTarget = false;
+        for(String word : words){
+            if(word.equals(target)){
+                containsTarget = true;
+                break;
+            }
+        }
+        
+        if(!containsTarget) return 0;
+        
         dfs(begin, target, words, 0);
+        
         
         return answer;
     }
     
-    public void dfs(String begin, String target, String[] words, int cnt){
+    static void dfs(String begin, String target, String[] words, int cnt){
+        
         if(begin.equals(target)){
-            answer = cnt;
+            answer = Math.min(answer, cnt);
             return;
         }
         
         
         for(int i=0; i<words.length; i++){
-            if(visited[i]){
-                continue;
-            }
+            String str = words[i];
             
-            int check = 0;
-            for(int j=0; j<begin.length(); j++){
-                if(begin.charAt(j) == words[i].charAt(j)){
-                    check++;
+            if(!visited[i]){
+                int c = 0;
+                
+                for(int j=0; j<begin.length(); j++){
+                   if(str.charAt(j) == begin.charAt(j)){
+                       c++;
+                   }
                 }
+                
+                
+                if(c == str.length() - 1){
+                    visited[i] = true;
+                    dfs(str, target, words, cnt + 1);
+                    visited[i] = false;
+                }
+                
             }
             
-            if(check == begin.length() - 1){
-                visited[i] = true;
-                dfs(words[i], target, words, cnt + 1);
-                visited[i] = false;
-            }
+            
         }
+       
+        
         
     }
 }
