bfa03bc89b3eecb2746ec74af53758b5b6457129
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-21 14:49:54 +0900

    [level 3] Title: N으로 표현, Time: 41.18 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,43 +2,54 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if (N == number) return 1; // N이 number와 같다면 1 반환
+        int answer = -1;
         
-        // DP 테이블 초기화 (1~8개의 N 사용 경우 저장)
-        List<HashSet<Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
+        List<Set<Integer>> list = new ArrayList<>();
+        
+        for(int i=0; i<=8; i++){
             list.add(new HashSet<>());
         }
-
-        // 1개 사용했을 때는 N 자체만 가능
+        
+        //초기화
         list.get(1).add(N);
-
-        // DP 테이블 채우기
-        for (int i = 2; i <= 8; i++) {
-            HashSet<Integer> currentSet = list.get(i);
+        
+        for(int i=2; i<=8; i++){
             
-            // N을 i개 이어붙인 숫자 추가 (ex: 5, 55, 555, 5555)
-            int num = Integer.parseInt(String.valueOf(N).repeat(i));
-            currentSet.add(num);
+            Set<Integer> now = list.get(i);
             
-            // j개 사용한 경우 + (i-j)개 사용한 경우 조합
-            for (int j = 1; j < i; j++) {
-                for (int num1 : list.get(j)) {
-                    for (int num2 : list.get(i - j)) {
-                        currentSet.add(num1 + num2);
-                        currentSet.add(num1 - num2);
-                        currentSet.add(num1 * num2);
-                        if (num2 != 0) currentSet.add(num1 / num2);
+            for(int j=1; j<=i; j++){
+                
+                Set<Integer> set1 = list.get(j);
+                Set<Integer> set2 = list.get(i-j);
+                
+                for(int a : set1){
+                    for(int b : set2){
+                        now.add(a + b);
+                        now.add(a - b);
+                        now.add(a * b);
+                        
+                        if( a > 0 && b > 0){
+                            now.add(a / b);
+                        }
                     }
                 }
             }
-
-            // 원하는 숫자가 있으면 정답 반환
-            if (currentSet.contains(number)) {
-                return i;
+            
+            now.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+        }
+        
+        
+        for(int i=1; i<=8; i++){
+            for(int a : list.get(i)){
+                if(a == number){
+                    return i;
+                }
             }
         }
-
-        return -1; // 8개 이하로 만들 수 없는 경우
+        
+        
+        
+        
+        return answer;
     }
 }
