aaa799b57f500e3e8bbf355a235cb1be39663b1a
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-24 09:47:47 +0900

    [Gold II] Title: 책 나눠주기, Time: 708 ms, Memory: 97252 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
@@ -1,74 +1,69 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    
-    static int n,m;
-    static ArrayList<Integer>[] list;
-    static boolean[] check;
-    static int[] matched;
+
+	static int n,m;
+	static int matched[]; //전체 매칭 상태 유지 배열
+	static boolean check[]; //한 명의 학생이 어떤 책에 접근했는지 체크
+	static List<List<Integer>> list;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<t; i++){
 
-        int t = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<t; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
+			list = new ArrayList<>();
+			matched = new int[n+1];
+			check = new boolean[n+1];
+			
+			for(int j=0; j<=m; j++){
+				list.add(new ArrayList<>());
+			}
 
-            list = new ArrayList[m+1];
-            check = new boolean[n+1];
-            matched = new int[n+1];
+			for(int j=1; j<=m; j++){
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
 
-            for(int j=1; j<=m; j++){
-                list[j] =  new ArrayList<>();
-            }
+				for(int k=a; k<=b; k++){
+					list.get(j).add(k);
+				}
+			}
 
-            for(int j=1; j<=m; j++){
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
+			int cnt = 0;
+			for(int j=1; j<=m; j++){
+				Arrays.fill(check, false);
+				if(dfs(j)) cnt++;
+			}
 
-                for(int k=a; k<=b; k++){
-                    list[j].add(k);
-                }
-            }
-
-            int count = 0;
-            for(int j=1; j<=m; j++){
-                Arrays.fill(check, false);
-                if(dfs(j)){
-
-                    count++;
-                }
-            }
-
-            System.out.println(count);            
-
-        }
-        
+			System.out.println(cnt);
+		}
+		
     }
 
-    static boolean dfs(int person){
-        for(int book : list[person]){
-            if(!check[book]){
-                check[book] = true;
 
-                if(matched[book] == 0 || dfs(matched[book])){
-                    matched[book] = person;
-                    return true;
-                }
-            }
-        }
+	static boolean dfs(int student){
+		for(int book : list.get(student)){
+			if(!check[book]){
+				check[book] = true;
 
-        return false;
-    }
+				if(matched[book] == 0 || dfs(matched[book])){
+					matched[book] = student;
+					return true;
+				}
+			}
+		}
 
+		return false;
+
+	}
 
 
 }
