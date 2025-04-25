13550522bc3cb7f307fb5df57e5ccec1f1688fb6
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-25 16:44:09 +0900

    [Bronze I] Title: 단어 공부, Time: 184 ms, Memory: 24452 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,39 +1,55 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int[] arr = new int[26]; // 영문자의 개수는 26개임
-		String s = in.next();
- 
-		for (int i = 0; i < s.length(); i++){
- 
-			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { // 대문자 범위
-				arr[s.charAt(i) - 'A']++;	// 해당 인덱스의 값 1 증가
-			}
-    
-			else {	// 소문자 범위
-				arr[s.charAt(i) - 'a']++;
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine().toUpperCase();
+		int[] alpha = new int[26];
+		int max = 0;
+
+		for(int i=0; i<str.length(); i++){
+			alpha[str.charAt(i) - 65]++;
+		}
+
+	
+		int max_val = 0;
+		for(int i=0; i<alpha.length; i++){
+
+			if(alpha[i] > max_val){
+				max = i;
+				max_val = alpha[i];
 			}
 		}
- 
- 
-		int max = -1;
-		char ch = '?';
- 
-		for (int i = 0; i < 26; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-				ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
-			}
-			else if (arr[i] == max) {
-				ch = '?';
+
+
+
+		int cnt = 0;
+		for(int i=0; i<alpha.length; i++){
+			if(max_val == alpha[i]){
+				cnt++;
 			}
 		}
- 
-		System.out.print(ch);
+
+		if(cnt > 1){
+			System.out.println("?");
+		}else{
+			int ans = max + 65;
+			System.out.println((char)ans);
+		}
+
+
+
+
+		
+
+		
 	}
- 
 }
+
+
+
+
