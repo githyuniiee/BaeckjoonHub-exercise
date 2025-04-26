64319abfe63a1d1df99667a599215c781fc9f1dd
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-04-26 14:31:33 +0900

    [Silver III] Title: 파일 정리, Time: 984 ms, Memory: 91036 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		HashMap<String, Integer> map = new HashMap<>();
+		TreeMap<String, Integer> map = new TreeMap<>();
 
 		for(int i=0; i<n; i++){
 			String str = br.readLine();
@@ -15,15 +15,7 @@ public class Main {
 			map.put(str, map.getOrDefault(str, 0) + 1);
 		}
 
-		List<String> list = new ArrayList<>();
-
 		for(String s : map.keySet()){
-			list.add(s);
-		}
-
-		Collections.sort(list);
-
-		for(String s : list){
 			System.out.println(s + " " + map.get(s));
 		}
 
