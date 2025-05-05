14ab33f8effec63c90f7dd980cffefd1a4aafccd
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-05 22:51:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.71 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -5,22 +5,22 @@ class Solution {
     static int[] parent;
     
     public int solution(int n, int[][] costs) {
-        
         int answer = 0;
         parent = new int[n];
-        
-        Arrays.sort(costs, (o1,o2) -> o1[2] - o2[2]);
         
         for(int i=0; i<n; i++){
             parent[i] = i;
         }
+        
+        Arrays.sort(costs, (o1,o2) -> o1[2] - o2[2]);
         
         for(int i=0; i<costs.length; i++){
             if(union(costs[i][0], costs[i][1])){
                 answer += costs[i][2];
             }
         }
-               
+        
+        
         return answer;
     }
     
