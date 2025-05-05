9c5588b18f60153522d22dcda6d197453a7b3f9f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-05 19:24:37 +0900

    [level 3] Title: 베스트앨범, Time: 1.81 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,45 +2,47 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
+        
         ArrayList<Integer> answer = new ArrayList<>();
         
-        Map<String, Integer> num = new HashMap<>(); // 장르별 총 재생 수
-        Map<String, HashMap<Integer, Integer>> music = new HashMap<>(); // 장르별 (고유번호, 재생수)
+        HashMap<String, Integer> total = new HashMap<>();
+        HashMap<String, HashMap<Integer, Integer>> map = new HashMap<>();
         
-        for (int i = 0; i < genres.length; i++) {
-            String genre = genres[i];
-            int play = plays[i];
-            
-            num.put(genre, num.getOrDefault(genre, 0) + play);
-            
-            HashMap<Integer, Integer> songMap = music.getOrDefault(genre, new HashMap<>());
-            songMap.put(i, play);
-            music.put(genre, songMap);
-        }
-
-        // 장르를 재생 수 내림차순으로 정렬
-        List<String> keySet = new ArrayList<>(num.keySet());
-        Collections.sort(keySet, (o1, o2) -> num.get(o2) - num.get(o1));
-        
-        for (String key : keySet) {
-            HashMap<Integer, Integer> songMap = music.get(key);
-            List<Integer> songList = new ArrayList<>(songMap.keySet());
-
-            // 해당 장르의 곡들을 재생 수 기준으로 정렬
-            Collections.sort(songList, (a, b) -> songMap.get(b) - songMap.get(a));
-            
-            answer.add(songList.get(0)); // 가장 많이 재생된 곡
-            if (songList.size() > 1) {
-                answer.add(songList.get(1)); // 두 번째로 많이 재생된 곡 (있다면)
+        for(int i=0; i<genres.length; i++){
+            if(!total.containsKey(genres[i])){
+                HashMap<Integer, Integer> m = new HashMap<>();
+                total.put(genres[i], plays[i]);
+                m.put(i, plays[i]);
+                map.put(genres[i], m);
+            }else{
+                total.put(genres[i], total.get(genres[i]) + plays[i]);
+                map.get(genres[i]).put(i, plays[i]);
             }
         }
         
-        // ArrayList를 int 배열로 변환 (스트림 없이)
-        int[] result = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            result[i] = answer.get(i);
+        List<String> keySet = new ArrayList<>(total.keySet());
+        Collections.sort(keySet, (o1,o2) -> total.get(o2) - total.get(o1));
+        
+        for(String key : keySet){
+            
+            HashMap<Integer, Integer> m = map.get(key);
+            
+            List<Integer> list = new ArrayList<>(m.keySet());
+            Collections.sort(list, (o1,o2) -> m.get(o2) - m.get(o1));
+            
+            answer.add(list.get(0));
+            
+            if(list.size() > 1){
+                answer.add(list.get(1));
+            }
         }
         
-        return result;
+        int[] ans = new int[answer.size()];
+        
+        for(int i=0; i<ans.length; i++){
+            ans[i] = answer.get(i);
+        }
+        
+        return ans;
     }
 }
