a50c3cbe7d9796b3339df00297f4612766b74826
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-06 11:42:42 +0900

    [level 3] Title: 단속카메라, Time: 14.81 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,19 +1,24 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
         
         Arrays.sort(routes, (o1,o2) -> o1[1] - o2[1]);
-        int answer = 0;
-        int cam = Integer.MIN_VALUE;
         
-        for(int[] route : routes){
-            if(cam < route[0]){
-                cam = route[1];
+        int now = Integer.MIN_VALUE;
+        int answer = 0;
+        
+        
+        for(int i=0; i<routes.length; i++){
+            
+            if(routes[i][0] <= now && now <= routes[i][1]){
+                continue;
+            }else{
+                now = routes[i][1];
                 answer++;
             }
             
         }
-        
         return answer;
     }
 }
