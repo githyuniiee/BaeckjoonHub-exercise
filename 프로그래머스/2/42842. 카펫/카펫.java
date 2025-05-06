b546476f0fc8a574a02610979c102b1d0d46b984
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-06 21:08:46 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int sum = brown + yellow;
+        int row = 0;
+        int col = 0;
         
-        for(int i=1; i<= sum; i++){ //가로의 길이
-            if(sum % i == 0){
-                int divideNum = sum / i; // 세로의 길이
-                if((i-2) * (divideNum -2) == yellow){
-                    answer[0] = divideNum;
-                    answer[1] = i;
-                    return answer;
-                }
+        for(int i=1; i<= yellow; i++){
+            int y_row = yellow / i;
+            int y_col = i;
+            
+            if(y_row < y_col) break;
+            
+            int b_row = y_row*2;
+            int b_col = (y_col + 2) *2;
+            
+            if(b_row + b_col == brown){
+                row = y_row + 2;
+                col = y_col + 2;
             }
         }
+        
+        answer[0] = row;
+        answer[1] = col;
         
         
         return answer;
