455e42d1a29c32b2f96ad258d855c84f91809cfb
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-07 14:08:08 +0900

    [level 2] Title: 프로세스, Time: 1.97 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,42 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
         
-        Queue<Integer> loc = new LinkedList<>();
-        Queue<Integer> pri = new LinkedList<>();
-        
-        for(int i=0; i< priorities.length; i++){
-            loc.add(i);
-            pri.add(priorities[i]);
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(i); // 인덱스만 저장 (문서 번호)
         }
-        
-        Arrays.sort(priorities);
-        int idx = priorities.length - 1;
-        
-        
-        while(idx >= 0){
-            
-            if(pri.peek() < priorities[idx]){
-                loc.add(loc.poll());
-                pri.add(pri.poll());
-            }
-            
-            if(pri.peek() == priorities[idx]){
-                
-                int currentLoc = loc.poll();
-                pri.poll();
-                answer++;
-                idx--;
-                if(currentLoc == location){
+
+        int answer = 0;
+
+        while (!q.isEmpty()) {
+            int current = q.poll(); // 문서 인덱스
+
+            // 현재 문서보다 우선순위 높은 문서가 큐에 있는지 확인
+            boolean hasHigher = false;
+            for (int idx : q) {
+                if (priorities[idx] > priorities[current]) {
+                    hasHigher = true;
                     break;
                 }
             }
-            
-            
-           
+
+            if (hasHigher) {
+                // 우선순위 높은 게 있으면 다시 뒤로
+                q.add(current);
+            } else {
+                // 인쇄 처리
+                answer++;
+                if (current == location) {
+                    return answer;
+                }
+            }
         }
-        
-        return answer;
+
+        return -1; // 이론상 도달하지 않음
     }
 }
