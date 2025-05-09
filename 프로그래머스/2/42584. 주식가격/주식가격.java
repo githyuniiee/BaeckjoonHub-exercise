2dd0c75d9da723773aed4d8e00c3eff8fe28d82f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-09 22:47:24 +0900

    [level 2] Title: 주식가격, Time: 35.86 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,23 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        
         Stack<Integer> stack = new Stack<>();
         
         for(int i=0; i<prices.length; i++){
             
+            int now = prices[i];
             
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+            while(!stack.isEmpty() && prices[stack.peek()] > now){
                 
-                int x = stack.pop();
-                answer[x] = i - x;  
+                    int idx = stack.pop();
+                    answer[idx] = i - idx;
+                
             }
+            
             stack.push(i);
         }
         
         while(!stack.isEmpty()){
-            
-            int x = stack.pop();
-            answer[x] = prices.length - x - 1;
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx -1;
         }
         
         return answer;
