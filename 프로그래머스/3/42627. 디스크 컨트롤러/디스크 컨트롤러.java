719c1c902432a05cfddcfaefc403e698d613aba2
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-09 18:18:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.44 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,39 +1,41 @@
 import java.util.*;
+
 class Solution {
-    public  int solution(int[][] jobs) {
+    public int solution(int[][] jobs) {
+        
+        //작업 시간 기준으로 오름차순 정렬
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        
+        
+        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
         int answer = 0;
-        int end = 0; //수행되고난 직후의 시간
-        int jobsIdx = 0; //jobs 배열의 인덱스
+        
         int count = 0;
+        int idx = 0;
+        int time = 0;
+        int totalWaitTime = 0;
         
-        
-        //받은 배열 정렬
-        Arrays.sort(jobs, (a,b)-> a[0] - b[0]);
-        
-        //처리 시간 오름차순으로 정렬
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        
-        while(count <jobs.length) {
+        //모든 작업이 처리될 때까지 반복
+        while(count < jobs.length){
             
-            //하나의 작업이 완료되는 시점까지 들어온 모든 요청을 큐에 넣음
-            while(jobsIdx < jobs.length && jobs[jobsIdx][0] <= end){
-                pq.add(jobs[jobsIdx++]);
+            //현재 시간 이전에 요청된 작업 우선순위 큐에 추가
+            while(idx < jobs.length && jobs[idx][0] <= time){
+                q.offer(jobs[idx++]);
             }
             
-            //큐가 비어있다면 작업 완료 이후에 다시 요청이 들어온다는 의미
-            if(pq.isEmpty()){
-                end = jobs[jobsIdx][0];
-            }else{
-                //가장 수행 시간이 짧은 요청부터 수행
-                int[] temp = pq.poll();
-                answer += temp[1] + end - temp[0];
-                end += temp[1];
+            if(q.isEmpty()){
+                time = jobs[idx][0];
+            }else {
+                //다음 작업
+                int[] job = q.poll();
+                
+                time += job[1];
+                
+                totalWaitTime += time - job[0];
+                
                 count++;
             }
-            
         }
-        
-        
-        return (int) Math.floor(answer/jobs.length);
+        return totalWaitTime / jobs.length;
     }
 }
