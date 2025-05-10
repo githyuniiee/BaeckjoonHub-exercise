08899de5b72148b19d1918c9010d7e14ad5ed762
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-10 11:21:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.28 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,39 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         
-        //작업 시간 기준으로 오름차순 정렬
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        //출발시점 기준 정렬
+        Arrays.sort(jobs, (o1,o2) -> o1[0] - o2[0]);
         
+        //실행시간 기준 정렬
+        Queue<int[]> q = new PriorityQueue<>((o1,o2) -> o1[1] - o2[1]);
         
-        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
-        int answer = 0;
+        int count = 0; //처리된 작업의 수
+        int idx = 0; //현재 처리할 작업의 인덱스
+        int time = 0; //현재 시간
+        int totalWaitTime = 0; //전체 작업의 대기 시간
         
-        int count = 0;
-        int idx = 0;
-        int time = 0;
-        int totalWaitTime = 0;
-        
-        //모든 작업이 처리될 때까지 반복
         while(count < jobs.length){
             
-            //현재 시간 이전에 요청된 작업 우선순위 큐에 추가
             while(idx < jobs.length && jobs[idx][0] <= time){
-                q.offer(jobs[idx++]);
+                q.offer(jobs[idx]);
+                idx++;
             }
             
+            //큐가 비어있다면, 다음 작업의 요청 시간으로 이동
             if(q.isEmpty()){
                 time = jobs[idx][0];
             }else {
-                //다음 작업
                 int[] job = q.poll();
                 
                 time += job[1];
                 
-                totalWaitTime += time - job[0];
+                totalWaitTime += time -job[0];
                 
                 count++;
             }
+            
         }
+            
         return totalWaitTime / jobs.length;
     }
 }
