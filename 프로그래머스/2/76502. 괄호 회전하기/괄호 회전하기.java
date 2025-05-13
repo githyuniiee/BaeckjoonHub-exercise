9b3bb6a1ab4d152d04ea766550e4a74cc59833e9
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-13 22:08:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 125.86 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,34 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+
         
         for(int i=0; i<s.length(); i++){
             
             Stack<Character> stack = new Stack<>();
             String str = s.substring(i,s.length()) + s.substring(0,i);
-            
             for(int j=0; j<str.length(); j++){
-              
-                char ch = str.charAt(j);
+                
                 
                 if(stack.isEmpty()){
-                    stack.push(ch);
+                    stack.push(str.charAt(j));
                 }else{
-                    if(stack.peek()=='(' && ch == ')'){
+                    if(stack.peek() == '(' && str.charAt(j) == ')'){
                         stack.pop();
-                    }else if(stack.peek()=='[' && ch == ']'){
+                    }else if(stack.peek() == '[' && str.charAt(j) == ']'){
                         stack.pop();
-                    }else if(stack.peek()=='{' && ch == '}'){
+                    }else if(stack.peek() == '{' && str.charAt(j) == '}'){
                         stack.pop();
                     }else{
-                        stack.push(ch);
+                        stack.push(str.charAt(j));
                     }
                 }
-            
             }
-             if(stack.isEmpty()) {answer++;}
-        }
-        
+            
+            if(stack.isEmpty()){
+               answer++; 
+            }
+        }        
         return answer;
     }
 }
