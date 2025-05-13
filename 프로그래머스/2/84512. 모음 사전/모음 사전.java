3c1bf4d2caaa41780c22f9a0d1b932cb7ac4f59f
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-13 17:24:52 +0900

    [level 2] Title: 모음 사전, Time: 19.22 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,31 @@
 import java.util.*;
+
 class Solution {
     
-    static String[] alpha = new String[] {"A", "E", "I", "O", "U"};
-    static List<String> list;
+    static char alpha[] = new char[]{'A','E','I','O', 'U'};
+    static int answer = 0;
+    static int idx = 0;
     
     public int solution(String word) {
-        int answer = 0;
-        list = new ArrayList<>();
-        dfs("", 0);
         
-        for(int i=0; i<list.size(); i++){
-            if(list.get(i).equals(word)){
-                answer = i;
-                break;
-            }
-        }
+        dfs("", word);
         return answer;
     }
     
-    static void dfs(String str, int len){
-        list.add(str);
-        if(len == 5){
-            return; 
+    static void dfs(String str, String word){
+        
+        if(str.length() > 5) return;
+        
+        if(str.equals(word)){
+            answer = idx;
+            return;
         }
         
-        for(int i=0; i<5; i++){
-            dfs(str + alpha[i], len +1);
+        idx++;
+        
+        for(int i=0; i<alpha.length; i++){
+            dfs(str + alpha[i], word);
         }
+        
     }
 }
