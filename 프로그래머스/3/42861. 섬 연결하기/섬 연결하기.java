69d5a2a3643f7751f558fb054d32718b72761e1d
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-15 20:20:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.68 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -6,6 +6,7 @@ class Solution {
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
+        
         parent = new int[n];
         
         for(int i=0; i<n; i++){
@@ -24,18 +25,19 @@ class Solution {
         return answer;
     }
     
-    static boolean union(int a, int b){
-        int pa = find(a);
-        int pb = find(b);
+    static boolean union(int x, int y){
+        int px = find(x);
+        int py = find(y);
         
-        if(pa == pb) return false;
-        parent[pa] = pb;
+        if(px == py) return false;
+        
+        parent[px] = py;
         return true;
     }
     
-    static int find(int x){
-        if(parent[x] == x) return x;
+    static int find(int a){
+        if(parent[a] == a) return a;
         
-        return find(parent[x]);
+        return find(parent[a]);
     }
 }
