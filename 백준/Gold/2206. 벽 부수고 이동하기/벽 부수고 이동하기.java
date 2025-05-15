b3ba2b2a603a1486cdda7ecb13fdb497d950f461
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-15 15:56:47 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 640 ms, Memory: 149500 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -3,55 +3,58 @@ import java.util.*;
 
 public class Main {
 
-	static int n,m;
+
 	static int[][] arr;
 	static boolean[][][] visited;
+	static int n,m;
+	static int answer = 0;
 
-	static int[] dy = new int[]{-1,0,0,1};
-	static int[] dx = new int[]{0,-1,1,0};
+	static int dy[] = new int[]{-1,0,0,1};
+	static int dx[] = new int[]{0,-1,1,0};
 
 	static class Point{
 		int y;
 		int x;
-		int cnt;
 		boolean breakWall;
-
-		Point(int y, int x, int cnt, boolean breakWall){
+		int cnt;
+		
+		Point(int y, int x, boolean breakWall, int cnt){
 			this.y = y;
 			this.x = x;
-			this.cnt = cnt;
 			this.breakWall = breakWall;
+			this.cnt = cnt;
+			
 		}
 	}
 
-
-
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-
 
 		arr = new int[n][m];
 		visited = new boolean[n][m][2];
 
 		for(int i=0; i<n; i++){
 			String str = br.readLine();
-			for(int j=0; j<m; j++){
+			for(int j=0; j<str.length(); j++){
 				arr[i][j] = str.charAt(j) - '0';
 			}
 		}
 
-		System.out.println(bfs());
+		answer = bfs(0,0);
 
+		System.out.println(answer);
+		
+	}
 
-    }
+	static int bfs(int y, int x){
 
-	static int bfs(){
 		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(0, 0, 1, false));
+		q.add(new Point(y, x, false, 1));
+		visited[y][x][0] = true;
 
 		while(!q.isEmpty()){
 			Point now = q.poll();
@@ -64,30 +67,33 @@ public class Main {
 				int ny = now.y + dy[i];
 				int nx = now.x + dx[i];
 
-				if(0<=ny && ny<n && 0<=nx && nx < m){
+				if(0<=ny && ny < n && 0<=nx && nx < m){
 
-					//벽 이미 깸
+					//벽을 깬 경우
 					if(now.breakWall){
-
+						//벽이 아닌 경우
 						if(arr[ny][nx] == 0 && !visited[ny][nx][1]){
 							visited[ny][nx][1] = true;
-							q.add(new Point(ny, nx, now.cnt + 1, true));
+							q.add(new Point(ny, nx, true, now.cnt + 1));
 						}
 					}else{
+					//벽을 깨지 않은 경우
 
-						if(arr[ny][nx] == 0 && !visited[ny][nx][0]){
+						if(arr[ny][nx] == 0 && !visited[ny][nx][0]){ //벽이 아닌 경우
 							visited[ny][nx][0] = true;
-							q.add(new Point(ny, nx, now.cnt + 1, false));
-						}else if(arr[ny][nx] == 1 && !visited[ny][nx][1]){
+							q.add(new Point(ny, nx, false, now.cnt + 1));
+						}else if(arr[ny][nx] == 1 && !visited[ny][nx][1]){ //벽인 경우
 							visited[ny][nx][1] = true;
-							q.add(new Point(ny, nx, now.cnt + 1, true));
+							q.add(new Point(ny, nx, true, now.cnt + 1));
 						}
-					}
+
+					}	
 				}
 			}
 		}
-		return -1;
-	}
 
+		return -1;
+
+	}
 
 }
