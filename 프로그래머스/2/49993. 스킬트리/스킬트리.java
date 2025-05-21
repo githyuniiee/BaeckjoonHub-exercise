4253181f1d67bffbb0e76d7d0d365c365f8abab6
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-05-21 09:07:58 +0900

    [level 2] Title: 스킬트리, Time: 2.37 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,22 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
+        
         int answer = 0;
         
         for(int i=0; i<skill_trees.length; i++){
             String[] str = skill_trees[i].split("");
-            String s = "";
+            String init = "";
             
             for(int j=0; j<str.length; j++){
+                
                 if(skill.contains(str[j])){
-                    s += str[j];
+                    init += str[j];
                 }
             }
             
-            if(skill.startsWith(s)){
+            if(skill.startsWith(init)){
                 answer++;
             }
-            
+           
         }
         return answer;
     }
