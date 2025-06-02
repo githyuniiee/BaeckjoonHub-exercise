f1661182af7fa0d3f28d71d32b298ab9bef743e8
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-06-02 09:54:37 +0900

    [level 3] Title: 여행경로, Time: 73.57 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,42 +2,31 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static ArrayList<String> list;
+    static ArrayList<String> answers = new ArrayList<>();
+    
     public String[] solution(String[][] tickets) {
         visited = new boolean[tickets.length];
-        list = new ArrayList<>();
         
-        for(int i=0; i<tickets.length; i++){
-            
-            if(tickets[i][0].equals("ICN")){
-                visited[i] = true;
-                dfs(1, tickets[i][1], tickets, "ICN");
-                visited[i] = false;
-            }
-        }
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         
-        Collections.sort(list);
-        
-        String[] answer = list.get(0).split(" ");
-        return answer;
+        dfs("ICN", "ICN", tickets, 0);
+
+        String[] result = answers.get(0).split(" ");
+        return result;
     }
     
-    static void dfs(int depth, String next, String[][] tickets, String str){
-        
-        
+    static void dfs(String curr, String path, String[][] tickets, int depth){
         if(depth == tickets.length){
-            str += " " + next;
-            list.add(str);
+            answers.add(path);
             return;
         }
         
-        for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0].equals(next) && !visited[i]){
+        for(int i = 0; i < tickets.length; i++){
+            if(!visited[i] && tickets[i][0].equals(curr)){
                 visited[i] = true;
-                dfs(depth + 1, tickets[i][1], tickets, str + " " + tickets[i][0]);
-                visited[i] = false;
+                dfs(tickets[i][1], path + " " + tickets[i][1], tickets, depth + 1);
+                visited[i] = false; // 백트래킹
             }
         }
-        
     }
 }
