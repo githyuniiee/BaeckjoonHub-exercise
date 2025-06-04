061f35765e9b39639380efefd22865237e0eeb52
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-06-04 09:46:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.98 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,50 @@
 import java.util.*;
+
 class Solution {
-    static ArrayList<Integer>[] graph;
-    static int min;
+    
+    static ArrayList<Integer>[] list;
     
     public int solution(int n, int[][] wires) {
-        graph = new ArrayList[n+1];
-        min = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
         
-        for(int i=1; i<=n; i++){
-            graph[i] = new ArrayList<>();
+        list = new ArrayList[n+1];
+        
+        for(int i=0; i<=n; i++){
+            list[i] = new ArrayList<>();
         }
         
         for(int i=0; i<wires.length; i++){
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
-            graph[v1].add(v2);
-            graph[v2].add(v1);
+            int start = wires[i][0];
+            int end = wires[i][1];
+            
+            list[start].add(end);
+            list[end].add(start);
         }
         
         for(int i=0; i<wires.length; i++){
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
+            int start = wires[i][0];
+            int end = wires[i][1];
+            
+            list[start].remove(Integer.valueOf(end));
+            list[end].remove(Integer.valueOf(start));
             
             boolean[] visited = new boolean[n+1];
             
-            graph[v1].remove(Integer.valueOf(v2));
-            graph[v2].remove(Integer.valueOf(v1));
+            int cnt = dfs(1, visited);
+            diff = Math.min(diff, (Math.abs(cnt - (n - cnt))));
             
-            int cnt = dfs(1,visited);
-            
-            int diff = Math.abs(cnt - (n - cnt));
-            min = Math.min(min, diff);
-            
-            graph[v1].add(v2);
-            graph[v2].add(v1);
+            list[start].add(end);
+            list[end].add(start);
         }
         
-        return min;
+        return diff;
     }
     
-    static int dfs(int v, boolean[] visited){
-        visited[v] = true;
+    static int dfs(int num, boolean[] visited){
+        visited[num] = true;
         int cnt = 1;
         
-        for(int next : graph[v]){
+        for(int next : list[num]){
             if(!visited[next]){
                 cnt += dfs(next, visited);
             }
