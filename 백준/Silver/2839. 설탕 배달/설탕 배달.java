a692698fc279435280036dc57b949586611f06ac
Bohyun <109260733+githyuniiee@users.noreply.github.com>
2025-06-06 21:10:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,32 +1,29 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
 
-
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int result = 0;
 
-        while(true){
-            if (n % 5 == 0) {
-                result += n/5;
-                break;
-            }
+		int[] dp = new int[5001];
 
-            n -= 3;
-            result++;
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
 
-            if (n < 0) {
-                result = -1;
-                break;
-            }
-        }
+		for(int i=3; i<=n; i++){
+			if(i>=3 && dp[i-3] != Integer.MAX_VALUE){
+				dp[i] = Math.min(dp[i], dp[i-3] + 1);
+			}
 
-        System.out.println(result);
+			if(i>=5 && dp[i-5] != Integer.MAX_VALUE){
+				dp[i] = Math.min(dp[i], dp[i-5] + 1);
+			}
+		}
 
+		dp[n] = dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
 
-
+		System.out.println(dp[n]);
     }
 }
